6bc79d5eead063b55bc60a75423c2aa0756f5e3b
Collin Didier <collin.didier80@gmail.com>
2021-01-10 17:06:27 -0600

    add to styles and create history stack

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/Accordion/IncomeTable/IncomeRow/IncomeRow.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/Accordion/IncomeTable/IncomeRow/IncomeRow.js
@@ -8,10 +8,19 @@ import {
     IconButton,
     TableCell, 
     TableRow,
+    Fade
 } from '@material-ui/core';
 
 
 const useStyles = makeStyles({
+    row: {
+        height: "38px",
+        maxHeight: "38px"
+    },
+    iconCell: {
+        maxWidth: "36px",
+        padding: "0px"
+    },
     iconButton: {
         marginRight: "11px",
         "&:hover" : {
@@ -20,6 +29,10 @@ const useStyles = makeStyles({
     },
     deleteIcon: {
         color: "red",
+        fontSize: "13px"
+    },
+    undoIcon: {
+        color: "lightgray",
     }
 })
 
@@ -28,14 +41,10 @@ const IncomeRow = (props) => {
 
     {/*  PROPS */}
     
-    const { 
-        source,
-        amount,
-        arrayIndex
-    } = props
 
     const {
         showIncomeDeleteIcons, 
+        setFourColumns,
     } = props.fromIncomeAccordion
     
     const { 
@@ -44,9 +53,16 @@ const IncomeRow = (props) => {
         toggleChanges,
         userMadeChanges,
         updateBudget,
-        budgetTicker,
         tick
     } = props.fromBudget
+
+    const {
+        source,
+        amount,
+        arrayIndex,
+        incomingDeletion,
+        setIncomingDeletion,
+    } = props.fromIncomeTable
 
 
     const monthly = Math.round(amount / 12)
@@ -54,7 +70,7 @@ const IncomeRow = (props) => {
 
     {/*  STATE  */}
     
-    const [ cellHistory, setCellHistory ] = useState([])
+    const [ cellAmountHistory, setCellAmountHistory ] = useState([])
     const [ showUndoIcon, setShowUndoIcon ] = useState(false)
     const [ iconShouldShow, setIconShouldShow ] = useState(false)
 
@@ -62,14 +78,48 @@ const IncomeRow = (props) => {
     {/*  useEffect */}
 
     useEffect(() => {
-        const newHistoryStack = checkHistoryStack(cellHistory, amount)
-        setCellHistory(newHistoryStack)
-        if(newHistoryStack.length > 1){
-            setShowUndoIcon(true)
+        // console.log("useEffect reached in income row")
+        // console.log('index:', arrayIndex)
+        // console.log("source:", source)
+        // console.log("incoming deletion", incomingDeletion)
+
+        // const deletion = (incomingDeletion >= 0)
+        // console.log(deletion)
+
+        if(incomingDeletion){
+            setCellAmountHistory([amount])
+            if (arrayIndex === newIncomes.length-1){
+                console.log("last elem")
+                setIncomingDeletion(false)
+            }
+            return
         }
-        // console.log("cellHistory:", cellHistory)
-        console.log("row info:", cellHistory, amount)
-    }, [amount])
+
+
+        const lengthOfStack = cellAmountHistory.length
+        const lastHistoryIndex = lengthOfStack - 1
+        const lastHistoryValue = lastHistoryIndex >= 0 ? cellAmountHistory[lastHistoryIndex] : null
+        const valueIsNew = lastHistoryIndex >= 0  ? (amount !== lastHistoryValue) : null
+        let nextLength 
+
+        if (lengthOfStack === 0) {
+            setCellAmountHistory([amount])
+            // arrayOfHistories.push([amount])
+            nextLength = 1
+        } else {
+            if (valueIsNew){
+                console.log("new value")
+                setCellAmountHistory([...cellAmountHistory, amount])
+                // let ArrayOfHistoriesCopy = [...arrayOfHistories]
+                // ArrayOfHistoriesCopy[arrayIndex] = [...cellAmountHistory, amount]
+                // updateArrayOfHistories(ArrayOfHistoriesCopy)
+                nextLength = lengthOfStack + 1
+            } else {
+                nextLength = lengthOfStack
+            }
+        }
+        renderUndoIcon(nextLength)
+    }, [amount, incomingDeletion])
 
 
     useEffect(() => {
@@ -94,97 +144,137 @@ const IncomeRow = (props) => {
     const handleDeleteIncome = (e) => {
         e.preventDefault()
         try {
+            // console.log("newIncomes:", newIncomes)
             const incomeIndex = e.currentTarget.id
-            let newIncomesCopy = newIncomes
+            let newIncomesCopy = [...newIncomes]
             newIncomesCopy.splice(incomeIndex, 1)
+            // console.log('newIncomesCopy after splice: ', newIncomesCopy)
             setNewIncomes(newIncomesCopy)
+            // this may not work for index 0
+            setIncomingDeletion(true)
+            // let ArrayOfHistoriesCopy = [...arrayOfHistories]
+            // ArrayOfHistoriesCopy.pop()
+            // updateArrayOfHistories(ArrayOfHistoriesCopy)
             if (!userMadeChanges) {
                 toggleChanges(true)
             }
             updateBudget(tick + 1)
-
         } catch (error) {
             console.log(error)
         }
     }
 
 
-    const checkHistoryStack = (cellHistory, amount) => {
-        const cellHasHistory = (cellHistory.length > 0)
-        if (cellHasHistory) {
-            const lastValue = cellHistory[cellHistory.length-1]
-            const cellWasChanged = (amount != lastValue)
-            if (cellWasChanged) {
-                return [...cellHistory, amount]
-            }
+    const renderUndoIcon = (newLength) => {
+        if (newLength >= 2) {
+            setShowUndoIcon(true)
+            setFourColumns(true)
         } else {
-            return [amount]
+            setShowUndoIcon(false)
+            setFourColumns(false)
         }
-        
     }
-
 
     const restoreLastValue = (e) => {
         e.preventDefault()
-        let historyCopy = cellHistory
-        const previousValue = historyCopy.pop()
-        setCellHistory(historyCopy)
-        setNewIncomes(previousValue)
+        let newIncomesCopy = newIncomes
+        let historyCopy = cellAmountHistory
+
+        // console.log("history copy:",  historyCopy)
+
+        const currentAmount = historyCopy.pop()
+        const previousAmount = historyCopy[historyCopy.length - 1]
+
+        // console.log("currentAmount:",  currentAmount)
+        // console.log("history after pop:",  historyCopy)
+
+        const previousRow = {
+            source: source, 
+            amount: previousAmount
+        }
+
+        newIncomesCopy[arrayIndex] = previousRow
+        setCellAmountHistory(historyCopy)
+
+        setNewIncomes(newIncomesCopy)
         updateBudget(tick + 1)
+    }
+
+    const propsForSource = {
+        defaultValue: source
+    }
+
+    const propsForMonthly = {
+        defaultValue: monthly,
+        isAnnual: false
+    }
+
+    const propsForAnnual = {
+        defaultValue: amount,
+        isAnnual: true
     }
 
 
     return (
-        <TableRow >
+        <TableRow
+            className={classes.row}
+        >
+            {/* {(!iconShouldShow && fourColumns) &&
+                <TableCell 
+                    size="small" 
+                    className={classes.iconCell}
+                >
+                </TableCell>
+            } */}
+                <TableCell 
+                    size="small" 
+                    className={classes.iconCell}
+                    style={{width: "30px"}}
+                >
+                    {iconShouldShow && (
+                        <Fade in={iconShouldShow}>
+                            {showIncomeDeleteIcons ? 
+                                <IconButton
+                                    className={classes.iconButton}
+                                    onClick={(e) => handleDeleteIncome(e)}
+                                    id={arrayIndex}
+                                >
+                                    <HighlightOffIcon 
+                                        className={classes.deleteIcon} 
+                                        fontSize="small"
+                                    />
+                                </IconButton>
 
-            {iconShouldShow && (
-                    <TableCell>
-                        {showIncomeDeleteIcons ? 
-                            <IconButton
-                                className={classes.iconButton}
-                                onClick={(e) => handleDeleteIncome(e)}
-                                id={arrayIndex}
-                            >
-                                <HighlightOffIcon 
-                                    className={classes.deleteIcon} 
-                                    fontSize="small"
-                                />
-                            </IconButton>
+                                :
 
-                            :
-
-                            <IconButton
-                                className={classes.iconButton}
-                                onClick={(e) => restoreLastValue(e)}
-                                id={arrayIndex}
-                            >
-                                <UndoIcon 
-                                    className={classes.deleteIcon} 
-                                    fontSize="small"
-                                />
-                            </IconButton>
-                        }
-                    </TableCell>
-                )
-            }
-
-
+                                <IconButton
+                                    className={classes.iconButton}
+                                    onClick={(e) => restoreLastValue(e)}
+                                    id={arrayIndex}
+                                >
+                                    <UndoIcon 
+                                        className={classes.undoIcon} 
+                                        fontSize="small"
+                                    />
+                                </IconButton>
+                            }
+                        </Fade>
+                    )}
+            </TableCell>
 
             <IncomeSourceCell 
                 {...props}
-                defaultValue={source} 
+                fromIncomeRow={{...propsForSource}} 
             />
             <IncomeNumberCell 
                 {...props}
                 align="right"
-                defaultValue={monthly} 
-                isAnnual={false}
+                fromIncomeRow={{...propsForMonthly}}
             />
             <IncomeNumberCell 
                 {...props}
                 align="right"
-                defaultValue={amount} 
-                isAnnual={true}
+                fromIncomeRow={{...propsForAnnual}}
             />
         </TableRow>
     )
