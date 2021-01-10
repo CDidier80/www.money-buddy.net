2b370a957fe210b4e283d92e08946b8dbef18676
Collin Didier <collin.didier80@gmail.com>
2021-01-10 17:06:02 -0600

    repackage props and add deletion flag to state

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/Accordion/IncomeTable/IncomeTable.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/Accordion/IncomeTable/IncomeTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IncomeRow from "./IncomeRow/IncomeRow"
 import IncomeHeaders from "./IncomeHeaders/IncomeHeaders"
 import { 
@@ -7,6 +7,7 @@ import {
     Table, 
     TableBody, 
     TableContainer, 
+    AccordionDetails
 } from '@material-ui/core';
 
 
@@ -28,43 +29,61 @@ const IncomeTable = (props) => {
     
     {/*  PROPS */}
 
-    const { newIncomes } = props.fromBudget
-    const { showIncomeDeleteIcons, toggleIncomeDeleteIcons } = props.fromIncomeAccordion
+    const { newIncomes } = props.fromBudget    
 
+
+    {/*  STATE */}
+
+    const [incomingDeletion, setIncomingDeletion] = useState(false)
 
     {/*  FUNCTIONS */}
 
     const classes = useStyles()
 
+    // const propsForHeaders = {
+
+    // }
 
     return (
-        <TableContainer 
-            className="tableContainer" 
-            component={Paper}
-        >
-            <Table className={classes.table} size="small" aria-label="a dense table">
-                <IncomeHeaders 
-                    showIncomeDeleteIcons={showIncomeDeleteIcons}
-                />
+        <AccordionDetails>
+            <TableContainer 
+                className={classes.tableContainer}
+                component={Paper}
+            >
+                <Table 
+                    className={classes.table} 
+                    size="small" 
+                >
 
-                <TableBody>
-                    {newIncomes.map((income, index) => {
-                        const { source, amount } = income
-                        const monthly = Math.round(amount / 12)
-                        return (
-                            <IncomeRow 
-                                {...props}
-                                key={index + 100000}
-                                source={source}
-                                amount={amount}
-                                monthly={monthly}
-                                arrayIndex={index}
-                            />
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+
+                    <TableBody>
+                    <IncomeHeaders 
+                        {...props}
+                    />
+                        {newIncomes.map((income, index) => {
+                            const { source, amount } = income
+                            let arrayIndex = index
+                            const monthly = Math.round(amount / 12)
+                            const propsForRows = {
+                                source,
+                                amount, 
+                                monthly,
+                                arrayIndex,
+                                incomingDeletion, 
+                                setIncomingDeletion,
+                            }
+                            return (
+                                <IncomeRow 
+                                    {...props}
+                                    key={index + 100000}
+                                    fromIncomeTable={{...propsForRows}}
+                                />
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </AccordionDetails>
     )
 }
 
