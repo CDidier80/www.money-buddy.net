9af19f7cddc9d71269fccf2a6b44508fc5a8ae78
Collin Didier <collin.didier80@gmail.com>
2021-01-11 00:46:46 -0600

    del unused styles

--- client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/CategoryAccordion/styles/useStyles.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/CategoryAccordion/styles/useStyles.js
@@ -1,13 +1,10 @@
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
+export const styles = {
     categoryWrapper: {
         width: "100%",
     },
     accordion: {
-        marginBottom: "20px",
+        marginBottom: "10px",
         width: "100%",
-
     },
     title: {
         flex: '1 1 100%',
@@ -16,35 +13,5 @@ const useStyles = makeStyles({
         maxWidth: "890px",
         margin: "auto"
     },
-    flexWrapper: {
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "center"
-    },
-    category: {
-        color: "#e6a824",
-        fontWeight: "500",
-        fontFamily: "Lato, sans-serif",
-    },
-    buttonGroup: {
-        // padding: "0"
-    },
-    button: {
-        fontSize: "9px",
-        fontWeight: "700",
-        fontFamily: "Lato, sans-serif",
-        color: "#e6a824",
-        padding: "0 5px 0 5px"
-    },
-    iconButton: {
-        marginRight: "11px",
-        "&:hover" : {
-            backgroundColor: "#ffcece65"
-        }
-    },
-    deleteIcon: {
-        color: "red",
-    }
-})
+}
 
-export default useStyles
