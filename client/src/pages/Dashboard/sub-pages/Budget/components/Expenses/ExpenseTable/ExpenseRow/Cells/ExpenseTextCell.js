3306834f77ef767fc301185c12b22c25c5f889f3
Collin Didier <collin.didier80@gmail.com>
2021-01-11 00:51:12 -0600

    improve performance of expense cells

---
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/ExpenseTable/ExpenseRow/Cells/ExpenseTextCell.js
@@ -0,0 +1,87 @@
+import { TableCell } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import "./editableCell.css"
+
+
+const ExpenseTextCell = (props) => {
+    
+    {/*  PROPS  */}
+
+    const { 
+        newCategories,
+        setNewCategories, 
+        toggleChanges,
+        userMadeChanges,
+        updateBudget, 
+        tick
+    } = props.fromBudget
+
+    const { category } = props.fromExpenseAccordion
+
+    const { expenseIndex, } = props.fromExpenseTable
+
+    const { 
+        defaultValue, 
+        categoryIndex, 
+    } = props.fromExpenseRow
+
+
+    {/*  STATE  */}
+
+    const [ newText, updateText ] = useState("")
+    const [ focused, setFocus ] = useState(false)
+
+    {/* FUNCTIONS */}
+
+    useEffect(() => {
+        updateText(defaultValue)
+    }, [defaultValue])
+
+
+    const updateNewCategories = (value) => {
+        let categoriesArrayCopy = [...newCategories]
+        let categoryCopy = {...category}
+        categoryCopy["expenses"][expenseIndex]['expense'] = value
+        categoriesArrayCopy[categoryIndex] = categoryCopy
+        setNewCategories(categoriesArrayCopy)
+        updateBudget(tick + 1)
+    }
+
+
+    const handleText = (e) => {
+        const { value } = e.target
+        updateText(value)
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+        document.activeElement.blur()
+        updateNewCategories(newText)
+        if (!userMadeChanges) {
+            toggleChanges(true)
+        }
+        updateBudget(tick + 1)
+        setFocus(false)
+        return false
+    }
+
+
+    return (
+        <TableCell>
+            <form onSubmit={(e) => submit(e)}>
+                <input 
+                    name="text-input"
+                    type="text" 
+                    value={newText} 
+                    className="editable-cell expense"
+                    onSelect={(e) => setFocus(true)}
+                    onChange={(e) => handleText(e)}
+                    onBlur={(e) => submit(e)}
+                >
+                </input>
+            </form>
+        </TableCell>
+        )
+}
+
+export default ExpenseTextCell
