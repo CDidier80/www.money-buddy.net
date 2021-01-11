c7c6023736ab562a670d1502600a58114838d341
Collin Didier <collin.didier80@gmail.com>
2021-01-11 00:48:23 -0600

    restructured and used more external componenets

--- client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/ExpenseAccordion/ExpenseAccordion.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/ExpenseAccordion/ExpenseAccordion.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import CategoryPopup from "../CategoryPopups/CategoryPopup"
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import CategoryAccordion from "../CategoryAccordion/CategoryAccordion"
-import { conditionlessStyles } from "./styles/useStyles"
+import ButtonsAddDelete from './componenets/ButtonsAddDelete'
+import AccordionDropdownTab from "./componenets/AccordionDropdownTab"
+import { unconditionalStyles } from "./styles/unconditionalStyles"
 import { 
-    Tooltip,
     makeStyles,
-    Button,
-    ButtonGroup,
-    Typography, 
     Accordion,
     AccordionDetails,
-    AccordionSummary,
-    AccordionActions
 } from '@material-ui/core';
+
 
 
 
@@ -21,34 +17,49 @@ const ExpenseAccordion = (props) => {
 
     {/*  PROPS */}
 
-    const { newCategories } = props.categoryHooks
+    const { newCategories } = props.fromBudget
+
+    console.log(newCategories)
     
 
     {/*  STATE  */}
     const [showAddCategoryPanel, toggleAddCategoryPanel] = useState(false)
     const [showDeleteIcons, toggleDeleteIcons] = useState(false)
+    const [opened, toggleOpened] = useState(false)
 
     // must keep -- optimizes performance
     const [renderExpAccordion, rerenderExpenseAccordian ] = useState(false)
 
 
-    {/*  FUNCTIONS  */}
+    {/*  useEffect  */}
 
     useEffect(() => {
     }, [renderExpAccordion])
+
+    
+
+    {/*  FUNCTIONS  */}
 
     const addCategory = (e) => {
         e.preventDefault()
         toggleAddCategoryPanel(true)
     }
 
+
     const setDeleteIcons = (e) => {
         e.preventDefault()
         toggleDeleteIcons(!showDeleteIcons)
     }
 
+
+    const handleExpansion = (e) => {
+        toggleOpened(!opened)
+        console.log("changed")
+    }
+
+
     const useStyles = makeStyles({
-        ...conditionlessStyles, 
+        ...unconditionalStyles, 
         deleteButton: {
             fontSize: "9px",
             fontWeight: "700",
@@ -61,6 +72,13 @@ const ExpenseAccordion = (props) => {
     const classes = useStyles()
 
 
+    const buttonsAddDeleteProps ={
+        addCategory,
+        setDeleteIcons,
+        showDeleteIcons
+    }
+
+
     return (
         <div>
             { showAddCategoryPanel && 
@@ -70,53 +88,31 @@ const ExpenseAccordion = (props) => {
                 />
             }
 
-            <Accordion className={classes.accordion}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-
-                    <Typography 
-                        className={classes.heading}
-                    >
-                        Expenses
-                    </Typography>
-                </AccordionSummary>
-
-                <AccordionDetails >
-                    <ButtonGroup className={classes.buttonGroup} 
-                        variant="text" 
-                        color="primary" 
-                        aria-label="text primary button group"
-                    >
-                        <Button 
-                            className={classes.button}
-                            onClick={(e) => addCategory(e)}
-                        >
-                            Add Category
-                        </Button>
-                        <Button 
-                            className={classes.deleteButton}
-                            onClick={(e) => setDeleteIcons(e)}
-                        >
-                            {showDeleteIcons ? "Cancel Delete" : "Delete Category"}
-                        </Button>
-                    </ButtonGroup>
-                </AccordionDetails>
-
-                {newCategories.map((category, index) => (
-                    <AccordionDetails key={`${20000 + index}`} >
-                        <CategoryAccordion 
-                            {...props} 
-                            category={category} 
-                            categoryIndex={index}
-                            showDeleteIcons={showDeleteIcons}
-                            rerenderExpenseAccordian={rerenderExpenseAccordian}
-                            renderExpAccordion={renderExpAccordion}
-                        />
-                    </AccordionDetails>
-                ))}
+            <Accordion 
+                className={classes.accordion}
+                onChange={(e)=>handleExpansion(e)}
+            >
+                <AccordionDropdownTab opened={opened}/>
+                <ButtonsAddDelete 
+                    fromExpenseAccordion={{...buttonsAddDeleteProps}}
+                />
+                {newCategories.map((category, index) => {
+                    const categoryAccordionProps = {
+                        category,
+                        categoryIndex: index,
+                        showDeleteIcons,
+                        rerenderExpenseAccordian,
+                        renderExpAccordion
+                    }
+                    return (
+                        <AccordionDetails key={`${20000 + index}`} >
+                            <CategoryAccordion 
+                                {...props} 
+                                fromExpenseAccordion={{...categoryAccordionProps}}
+                            />
+                        </AccordionDetails>
+                    )
+                })}
             </Accordion>
         </div>
     )
