6df5364786fd6ce35bb998d6467bdc1a2c91d04f
Collin Didier <collin.didier80@gmail.com>
2021-01-11 00:45:58 -0600

    cat delete icon

---
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Expenses/CategoryAccordion/components/CategoryDeleteIcon.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff'
+import { IconButton, makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    iconButton: {
+        marginRight: "11px",
+        "&:hover" : {
+            backgroundColor: "#ffcece65"
+        }
+    },
+    deleteIcon: {
+        color: "red",
+    }
+})
+
+const CategoryDeleteIcon = (props) => {
+
+    const {
+        toggleCategoryDeletePopup,
+        showCategoryDeletePopup
+    } =  props.fromCategoryAccordion
+    
+    const classes = useStyles()
+
+    const handleDeleteIcon = (e) => {
+        e.preventDefault()
+        toggleCategoryDeletePopup(!showCategoryDeletePopup)
+    }
+
+    return (
+        <IconButton
+            className={classes.iconButton}
+            onClick={(e) => handleDeleteIcon(e)}
+        >
+            <HighlightOffIcon 
+                className={classes.deleteIcon} 
+                fontSize="small"
+            />
+        </IconButton>
+    )
+}
+
+export default CategoryDeleteIcon
