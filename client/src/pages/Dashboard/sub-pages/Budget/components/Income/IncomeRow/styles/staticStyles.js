dde1c48f1638099d6ade93d17d97c256807fbebc
Collin Didier <collin.didier80@gmail.com>
2021-01-14 11:46:30 -0600

    income row and subcomponents

---
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeRow/styles/staticStyles.js
@@ -0,0 +1,19 @@
+export const unconditionalStyles = {
+    iconCell: {
+        maxWidth: "36px",
+        padding: "0px"
+    },
+    iconButton: {
+        marginRight: "11px",
+        "&:hover" : {
+            backgroundColor: "#ffcece65"
+        }
+    },
+    deleteIcon: {
+        color: "red",
+        fontSize: "13px"
+    },
+    undoIcon: {
+        color: "lightgray",
+    }
+}
