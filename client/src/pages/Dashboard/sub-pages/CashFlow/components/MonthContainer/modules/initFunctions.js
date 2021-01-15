5e62a51b51dbd4ccf6e057a33fc30176373a77c0
Collin Didier <collin.didier80@gmail.com>
2021-01-15 17:31:47 -0600

    move directory

---
+++ client/src/pages/Dashboard/sub-pages/CashFlow/components/MonthContainer/modules/initFunctions.js
@@ -0,0 +1,24 @@
+export const initTotalOutflow = (flowcategories) => {
+    // console.log("CATEGORIES RECEIVED FROM PROPS (initNameTotals()):", categories)
+    let totalOutflows = 0
+    flowcategories.forEach((flowcategory) => {
+        const { outflows } = flowcategory
+        if (outflows.length > 0) {
+            const total = outflows.reduce((accumulator, currentValue) => {
+                return accumulator += currentValue.amount
+            }, 0)
+            totalOutflows += total
+        }
+    })
+    return totalOutflows
+}
+
+
+export const initTotalInflow = (inflows) => {
+    let inflowTotal = 0
+    for (let i = 0; i <= inflows.length -1; i++) {
+        const inflowItem = inflows[i]["amount"]
+        inflowTotal += inflowItem
+    }
+    return(inflowTotal)
+}
