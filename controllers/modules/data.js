6e192412ff895abb1a8fb56369593b26fa6d8dc3
Collin Didier <collin.didier80@gmail.com>
2021-01-14 19:10:39 -0600

    add defaultMonths to data.js

---
+++ controllers/modules/data.js
@@ -0,0 +1,62 @@
+const defaultCategories = {
+    "Housing & Utilities": [
+        "Rent",
+        "Electricity",
+        "Water",
+        "Sewage",
+        "Gas",
+        "Trash Collection",
+        "Miscellanious"
+    ],
+
+    "Living" : [
+        "Groceries",
+        "Phone Bill",
+        "Clothing"
+
+    ],
+    "Medical & Wellness" : [
+        "Perscription Medication",
+        "Primary Care Visits",
+        "Health Insurance Premium",
+        "Gym Membership"
+    ],
+
+    "Vehicle & Transportation" : [
+        "Car Payment"
+    ],
+
+    "Debt" : [
+        
+    ],
+    "Taxes" : [
+    ],
+
+    "Vacation & Travel" : [
+
+    ],
+    "Holidays & Gifts" : [
+        "Christmas"
+    ],
+}
+
+
+const defaultMonths = [
+    "January",
+    "February",
+    "March", 
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+module.exports = {
+    defaultCategories,
+    defaultMonths
+}
