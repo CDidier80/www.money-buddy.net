d27a7273bfc6d7d8029d4730e845c0c7666760fd
Collin Didier <collin.didier80@gmail.com>
2021-01-14 19:10:24 -0600

    new cashflow related controller logs added

--- controllers/logs.js
+++ controllers/logs.js
@@ -79,6 +79,86 @@ exports.ControllerLoggers = {
         return
     }, 
 
+    CashflowControllerLog : (controllerFunction, req) => {
+        const show = true
+        if (show){
+            console.log(`Request arrived in CashflowController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params)
+        } 
+        return
+    },
+
+    CashflowControllerErrorLog : (controllerFunction, error) => {
+        const show = true
+        if (show){
+            console.log(`TRY{}CATCH{} ERROR --> FILE: CashflowController.js -->  FUNCTION: ${controllerFunction.name}  --> MESSAGE:`, error)
+        }
+        return
+    }, 
+
+    MonthControllerLog : (controllerFunction, req) => {
+        const show = true
+        if (show){
+            console.log(`Request arrived in MonthController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params)
+        } 
+        return
+    },
+
+    MonthControllerErrorLog : (controllerFunction, error) => {
+        const show = true
+        if (show){
+            console.log(`TRY{}CATCH{} ERROR --> FILE: MonthController.js -->  FUNCTION: ${controllerFunction.name}  --> MESSAGE:`, error)
+        }
+        return
+    }, 
+
+    InflowControllerLog : (controllerFunction, req) => {
+        const show = true
+        if (show){
+            console.log(`Request arrived in InflowController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params)
+        } 
+        return
+    },
+
+    InflowControllerErrorLog : (controllerFunction, error) => {
+        const show = true
+        if (show){
+            console.log(`TRY{}CATCH{} ERROR --> FILE: InflowController.js -->  FUNCTION: ${controllerFunction.name}  --> MESSAGE:`, error)
+        }
+        return
+    }, 
+
+    FlowcategoryControllerLog : (controllerFunction, req) => {
+        const show = true
+        if (show){
+            console.log(`Request arrived in FlowcategoryController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params)
+        } 
+        return
+    },
+
+    FlowcategoryControllerErrorLog : (controllerFunction, error) => {
+        const show = true
+        if (show){
+            console.log(`TRY{}CATCH{} ERROR --> FILE: FlowcategoryController.js -->  FUNCTION: ${controllerFunction.name}  --> MESSAGE:`, error)
+        }
+        return
+    }, 
+
+    OutflowControllerLog : (controllerFunction, req) => {
+        const show = true
+        if (show){
+            console.log(`Request arrived in OutflowController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params)
+        } 
+        return
+    },
+
+    OutflowControllerErrorLog : (controllerFunction, error) => {
+        const show = true
+        if (show){
+            console.log(`TRY{}CATCH{} ERROR --> FILE: OutflowController.js -->  FUNCTION: ${controllerFunction.name}  --> MESSAGE:`, error)
+        }
+        return
+    }, 
+
 
 }
 
