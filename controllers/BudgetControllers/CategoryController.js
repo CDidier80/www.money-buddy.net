0f46296f93d4beb275143264f71e9aad6f93dd95
Collin Didier <collin.didier80@gmail.com>
2021-01-14 19:09:07 -0600

    create new directory for budget-related controllers

---
+++ controllers/BudgetControllers/CategoryController.js
@@ -0,0 +1,109 @@
+const { Category, Expense } = require('../../models')
+const { ControllerLoggers } = require('../logs')
+const {defaultCategories} = require("../modules/data")
+// const { Op, literal, fn, col  } = require('sequelize')
+const log = ControllerLoggers.CategoryControllerLog 
+const errorLog = ControllerLoggers.CategoryControllerErrorLog
+
+
+/**
+ * Assign the project to an employee.
+ * @param {Object} req.body - The category object.
+ * @param {String} req.body.name - The name of the category.
+ * @param {Integer} req.body.budgetId - The id of the budget that has the category.
+ */
+const CreateCategory = async (req, res) => {
+    log(CreateCategory, req)
+    try {
+        const category = await Category.create(req.body)
+        res.send(category)
+    } catch (error) {
+        errorLog(CreateCategory, error) 
+    }
+}
+
+const RestoreDefaultCategories = async (req, res) => {
+    log(RestoreDefaultCategories, req)
+
+
+    try {
+        const { budgetId } = req.body
+        await Category.destroy({where:{}})
+        for (let category in defaultCategories) {
+            const newCategory = await Category.create({budgetId: budgetId, name: category})
+            const { id: categoryId } = newCategory
+            const expenses = defaultCategories[category]
+            expenses.forEach( async (expense) => {
+                await Expense.create({categoryId, expense: expense, amount: 0})
+            })
+        }
+        res.send({message: "Defaults restored"})
+        
+    } catch (error) {
+        errorLog(RestoreDefaultCategories, error)
+    }
+} 
+
+
+const GetOneCategory = async (req, res) => {
+    log(GetOneCategory, req)
+    try {
+        const { categoryId } = req.body
+        const category = await Category.findOne({
+            where: {
+                id: categoryId
+            }
+        })
+        res.send(category)
+    } catch (error) {
+        errorLog(GetOneCategory, error) 
+    }
+}
+
+const ReadBudgetCategories = async (req, res) => {
+    log(ReadBudgetCategories, req)
+    try {
+        const { budgetId } = req.body
+        const categories = await Category.findAll({
+            where: { 
+                budget_id: budgetId 
+            }
+        })
+        res.send(categories)
+        
+    } catch (error) {
+        console.log(error)
+        errorLog(ReadBudgetCategories, error)
+    }
+}
+
+
+/**
+ * Assign the project to an employee.
+ * @param {Object} req.body - The category object.
+ * @param {String} req.body.name - The name of the category.
+ * @param {Integer} req.body.budgetId - The id of the budget that has the category.
+ **/
+const DeleteCategoriesFromBudget = async (req,res) => {
+    log(DeleteCategoriesFromBudget, req)
+    try {
+        const { budgetId } = req.body
+        Category.destroy({
+            where: { 
+                budget_id: budgetId 
+            }
+        })
+        res.send({message: "delete finished"})
+        
+    } catch (error) {
+        errorLog(DeleteCategoriesFromBudget, error)
+    }
+}
+
+module.exports = {
+    CreateCategory,
+    GetOneCategory,
+    ReadBudgetCategories,
+    DeleteCategoriesFromBudget,
+    RestoreDefaultCategories
+}
---
+++ controllers/BudgetControllers/IncomeController.js
@@ -0,0 +1,73 @@
+const { Income } = require('../../models')
+const { ControllerLoggers } = require('../logs')
+// const { Op, literal, fn, col  } = require('sequelize')
+const log = ControllerLoggers.IncomeControllerLog 
+const errorLog = ControllerLoggers.IncomeControllerErrorLog
+
+const CreateIncome = async (req, res) => {
+    log(CreateIncome, req)
+    try {
+        const income = await Income.create(req.body)
+        res.send(income)
+    } catch (error) {
+        errorLog(CreateIncome, error) 
+    }
+}
+
+const CreateManyIncomes = async (req, res) => {
+    console.log("req: ", req)
+    log(CreateManyIncomes, req)
+    try {
+        const {incomes, budgetId} = req.body
+        const newIncomes = incomes.map(async (income) => {
+            const incomeToCreate = {
+                budgetId, 
+                source: income.source, 
+                amount: income.amount
+            }
+            const newIncome = await Income.create(incomeToCreate)
+            return newIncome
+        })
+        return newIncomes
+    } catch (error) {
+        errorLog(CreateManyIncomes, error)  
+    }
+}
+
+
+const GetOneIncome = async (req, res) => {
+    log(GetOneIncome, req)
+    try {
+        const { incomeId } = req.body
+        const income = await Income.findOne({
+            where: {
+                id: incomeId
+            }
+        })
+        res.send(income)
+    } catch (error) {
+        errorLog(GetOneIncome, error) 
+    }
+}
+
+const ReadBudgetIncomes = async (req, res) => {
+    log(ReadBudgetIncomes, req)
+    try {
+        const { budgetId } = req.body
+        const incomes = await Income.findAll({
+            where: {
+                budget_id: budgetId
+            }
+        })
+        res.send(incomes)
+    } catch (error) {
+        errorLog(ReadBudgetIncomes, error) 
+    }
+}
+
+module.exports = {
+    CreateIncome,
+    GetOneIncome,
+    ReadBudgetIncomes,
+    CreateManyIncomes
+}
