e6983caae0f2ccff44ecef8ed390c3c23fc0f0dc
Collin Didier <collin.didier80@gmail.com>
2021-01-14 23:19:54 -0600

    fix path, add CreateEntireCashflow method

--- client/src/Services/CashflowService.js
+++ client/src/Services/CashflowService.js
@@ -6,7 +6,7 @@ const errorLog = ServiceLoggers.CashflowServiceErrorLog
 export const ReadEntireCashflow = async (body,  params) =>  {
     try {
         log(ReadEntireCashflow, body, params)
-        const response = await ApiClient.post(`cashflow/entirecashflow`, body)
+        const response = await ApiClient.post(`cashflows/entirecashflow`, body)
         return response.data
     } catch (error) {
         errorLog(ReadEntireCashflow, error)
@@ -16,10 +16,21 @@ export const ReadEntireCashflow = async (body,  params) =>  {
 export const UpdateEntireCashflow = async (body,  params) =>  {
     try {
         log(UpdateEntireCashflow, body, params)
-        const response = await ApiClient.put(`cashflow/update`, body)
+        const response = await ApiClient.put(`cashflows/update`, body)
         console.log(response.data)
         return response.data
     } catch (error) {
         errorLog(UpdateEntireCashflow, error)
+    }
+}
+
+export const CreateEntireCashflow = async (body,  params) =>  {
+    try {
+        log(CreateEntireCashflow, body, params)
+        const response = await ApiClient.post(`cashflows/initaccount`, body)
+        console.log("response.data", response.data)
+        return response.data
+    } catch (error) {
+        errorLog(CreateEntireCashflow, error)
     }
 }
