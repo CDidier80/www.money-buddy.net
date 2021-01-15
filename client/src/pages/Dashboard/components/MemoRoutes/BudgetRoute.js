62f5400681b9b3165319f377e2e1235bbe652545
Collin Didier <collin.didier80@gmail.com>
2021-01-14 19:01:53 -0600

    made separate files for memoized routes

---
+++ client/src/pages/Dashboard/components/MemoRoutes/BudgetRoute.js
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+import { Route } from 'react-router-dom'
+import Budget from "../../sub-pages/Budget/Budget"
+
+const BudgetRoute = memo((props) => {
+    
+    return (
+        <Route 
+            component={ () => ( 
+                <Budget {...props}/> 
+        )} 
+    />
+    )
+},(prevProps, nextProps) => {
+    // returning a true value causes component to skip rerender
+    // if the cause of rerender is a change in ticker (caused by collapsing/opening menu),
+    // the route should not rerender
+    return (prevProps.ticker !== nextProps.ticker)
+})
+
+export default BudgetRoute
