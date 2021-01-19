6a20be171252cd11c53567a2a8dde27006de325f
Collin Didier <collin.didier80@gmail.com>
2021-01-19 12:44:42 -0600

    add dataset creation functions

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/CashflowDevelopment.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/CashflowDevelopment.js
@@ -7,24 +7,76 @@ import "./styles/CashFlow.css"
 
 const CashflowDevelopment = (props) => {
 
-    console.log(props)
+    // console.log(props)
 
+    /* ----------------------- STATE ----------------------- */
     const [newMonths, setMonths] = useState("")
+    const [inflows, setInflows] = useState("")
+    const [outflows, setOutflows] = useState("")
+    const [cashReserves, setCashReserves] = useState("")
+
     const [loaded, setLoaded] = useState(false)
     const [tick, incrementTicker] = useState(0)
+
 
     useEffect(() => {
         const generatedMonths = generateMonths()
         setMonths(generatedMonths)
+        const [
+            inflows,
+            outflows, 
+            adjustedCashReserves
+        ] = createDatasets(generatedMonths)
+
+        setInflows(inflows)
+        setOutflows(outflows)
+        setCashReserves(adjustedCashReserves)
+
     }, [tick])
 
     useEffect(() => {
-        if (newMonths !== "") {
+        if (
+            newMonths    !== ""  |
+            inflows      !== ""  |
+            outflows     !== ""  |
+            cashReserves !== "" 
+        ){
             setLoaded(true)
         }
 
-    }, [newMonths])
+    },  
+    [
+        newMonths,
+        inflows,
+        outflows, 
+        cashReserves
+    ])
 
+
+    /* ----------------------- FUNCTIONS ----------------------- */
+
+    const createDatasets = (months) => {
+
+        let inflows = []
+        let outflows = []
+        let monthlyCashReserve = [1000]
+
+        months.forEach(month => {
+            const index = monthlyCashReserve.length - 1
+            const lastCashReserve = monthlyCashReserve[index]
+            const totalInflows = month.totalInflows
+            const totalOutflows = month.totalOutflows
+            const monthlyCashflow = totalInflows - totalOutflows
+            const newCashReserve = lastCashReserve + monthlyCashflow
+            inflows.push(totalInflows)
+            outflows.push(totalOutflows)
+            monthlyCashReserve.push(newCashReserve)
+        })
+        let adjustedCashReserves = monthlyCashReserve.slice(1)
+        return [inflows, outflows, adjustedCashReserves]
+    }
+
+    /* ----------------------- PROPS FOR CHILDREN ----------------------- */
 
     const headerProps = {
         setLoaded, 
@@ -32,9 +84,20 @@ const CashflowDevelopment = (props) => {
         incrementTicker,
     }
 
-    const paginatingContainerProps = {
-        newMonths
+    const chartProps = {
+        inflows,
+        outflows, 
+        cashReserves,
     }
+
+    const paginatingContainerProps = {
+        newMonths,
+        inflows,
+        outflows, 
+        cashReserves
+    }
+
+    /* -------------------------- JSX -------------------------- */
 
     return ( !loaded ? <div></div> : 
 
@@ -42,7 +105,9 @@ const CashflowDevelopment = (props) => {
             <CashflowHeader 
                 fromCashflowDevelopment={{...headerProps}}
             />
-            <Chart months={newMonths}/>
+            <Chart 
+                fromCashflowDevelopment={{...chartProps}}
+            />
             <PaginatingContainer 
                 {...props}
                 fromCashflowDevelopment={{...paginatingContainerProps}}
