7e36c7087034844c63152c8d07fd5a170ebdf608
Collin Didier <collin.didier80@gmail.com>
2021-01-19 12:47:56 -0600

    restructure jsx

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/PaginatingContainer/PaginatingContainer.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/PaginatingContainer/PaginatingContainer.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import MonthContainer from "../MonthContainer/MonthContainer"
+import NextAndBackIcons from "./NextAndBackIcons"
 import "./styles/paginator.css"
 
 const PaginatingContainer = (props) => {
-    console.log(props)
-    
     // console.log(props)
     const { newMonths } = props.fromCashflowDevelopment
 
     return (
-        <div className="paginating-container">
-            {newMonths.map((month, index) => {
-                    const monthContainerProps = {
-                        thisMonth: {...month},
-                        monthIndex: index,
-                    }
-                    // console.log(monthContainerProps)
-                return (
-                    <MonthContainer 
-                        className="month-container"
-                        key={`ABC${index}`}
-                        {...props}
-                        fromPaginatingContainer={{...monthContainerProps}}
-                    />
-                )
-                })}
+        <div
+            className="paginating-container"
+        >
+            <NextAndBackIcons />
+            <div className="monthContainer-wrapper">
+                {newMonths.map((month, index) => {
+                        const monthContainerProps = {
+                            thisMonth: month,
+                            monthIndex: index,
+                        }
+                    return (
+                        <MonthContainer 
+                            className="month-container"
+                            key={`ABC${index}`}
+                            {...props}
+                            fromPaginatingContainer={{...monthContainerProps}}
+                        />
+                    )
+                    })}
+            </div>
         </div>
     )
 }
