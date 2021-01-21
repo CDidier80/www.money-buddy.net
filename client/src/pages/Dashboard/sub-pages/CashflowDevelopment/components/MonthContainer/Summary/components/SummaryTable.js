44eb3a44d8bd38fb7bcb3bc2ca914fafef7dd779
Collin Didier <collin.didier80@gmail.com>
2021-01-21 14:34:45 -0600

    restructure rows into micro-tables

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Summary/components/SummaryTable.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Summary/components/SummaryTable.js
@@ -132,22 +132,12 @@ const SummaryTable = (props) => {
                 className={classes.tableContainter}
                 component={Paper}
             >
-                <Table 
-                    // className={classes.table} 
-                    size="small" 
-                    aria-label="a dense table"
-                >
-                    <TableBody 
-                        className={classes.tableBody}
-                    >
-                        <FlowRows 
-                            fromSummaryTable={{...flowRowsProps}}
-                        />
-                        <CashRows 
-                            fromSummaryTable={{...cashRowsProps}}
-                        />
-                    </TableBody>
-                </Table>
+                <FlowRows 
+                    fromSummaryTable={{...flowRowsProps}}
+                />
+                <CashRows 
+                    fromSummaryTable={{...cashRowsProps}}
+                />
             </TableContainer>
         </AccordionDetails>
 
