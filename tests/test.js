443239e35e8acd31c5690e2e537ffbc1d0028a09
Collin Didier <collin.didier80@gmail.com>
2021-01-21 14:29:19 -0600

    test switch

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,11 @@
-console.log(typeof [])
-console.log([] == "undefined")
+switch (true) {
+    case true:
+        console.log("big")
+    case false:
+        console.log("big")
+    case true:
+        console.log("big")
+
+    default:
+        (()=>null)()
+}
