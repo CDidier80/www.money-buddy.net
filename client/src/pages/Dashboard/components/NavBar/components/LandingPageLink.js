49f2dfb416a6a749a172e0b1a7c4fea049a09d4a
Collin Didier <collin.didier80@gmail.com>
2021-01-23 11:35:12 -0600

    new component for landing page link

---
+++ client/src/pages/Dashboard/components/NavBar/components/LandingPageLink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LandingPageLink = () => 
+
+        <Link to="/" className="title-link">
+            <span className="money">Money</span>
+            Buddy
+        </Link> 
+
+export default LandingPageLink
