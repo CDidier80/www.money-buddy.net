af57ebe46c4dfb1661a5993bc941eb2c89a085fd
Collin Didier <collin.didier80@gmail.com>
2021-01-23 11:38:42 -0600

    add media queries

--- client/src/pages/Dashboard/sub-pages/Budget/components/DonutWidget/DoughnutChart.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/DonutWidget/DoughnutChart.js
@@ -1,60 +1,58 @@
 import { useMediaQuery } from '@material-ui/core'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Doughnut } from 'react-chartjs-2'
 import { 
     createData,
     createOptions 
     } from "./modules/donutFunctions"
 import "./modules/useResponsiveStyle"
-import useResponsiveStyle from './modules/useResponsiveStyle'
 import "./styles/donutStyles.css"
-
-
-
 
 
 
 const DoughnutChart = (props) => {
 
+    /* ---------------------- init MEDIA QUERIES ----------------------- */
 
-    
-    // console.log("RERENDERED")
+    const mq1 = useMediaQuery('(min-width:1400px)')
+    const mq2 = useMediaQuery('(min-width:1100px)')
+    const mq5 = useMediaQuery('(min-width:862px)')
+    const mq3 = useMediaQuery('(min-width:800px)')
+    const mq4 = useMediaQuery('(min-width:600px)')
+    const mq6 = useMediaQuery('(min-width:1800px)')
+
+    const mq1Ref = useRef("")
+    const mq2Ref = useRef("")
+    const mq3Ref = useRef("")
+    const mq4Ref = useRef("")
+    const mq5Ref = useRef("")
+    const mq6Ref = useRef("")
+
+    useEffect(() => {
+        mq1Ref.current = mq1
+        mq2Ref.current = mq2
+        mq3Ref.current = mq3
+        mq4Ref.current = mq4
+        mq5Ref.current = mq5
+        mq6Ref.current = mq6
+    })
+
 
     /* ---------------------------- PROPS ------------------------------ */
 
     const { categoryNames, categoryTotals} = props.fromBudget
 
-    
-
 
     /* ---------------------------- STATE ------------------------------ */
+
     const [monthly, setMonthly] = useState(false)
-
-
-
-
-
-
-    /* ---------------------------- VARIABLES ------------------------------ */
-
 
     const DATA = createData(categoryNames, categoryTotals, monthly)
 
-    const mq1 = useMediaQuery('(min-width:1500px)')
-    const mq2 = useMediaQuery('(min-width:1000px)')
-    const mq3 = useMediaQuery('(min-width:600px)')
 
-    const mediaQueries = {
-        min_width_1500px: mq1,
-        min_width_1000px: mq2,
-        min_width_600px: mq3,
-    }
-
-    const options = createOptions(mediaQueries)
-
-    return (
+    return ( 
         <div 
-        className="gradient-wrapper donut"
+            className="gradient-wrapper donut"
         >
             <div 
                 className="budget-top-widgets donut"
@@ -62,33 +60,28 @@ const DoughnutChart = (props) => {
                 <h3 className="donut-header">
                     Distribution of Spending
                 </h3>
-                <div style={{width:"100%", position: "relative"}}>
-                    <Doughnut 
-                        data={DATA}
-                        options={
-                            {...options,
-                            tooltips: {        
-                                callbacks: {
-                                    label:  (tooltipItem, data) => {
-                                        const { datasetIndex } = tooltipItem
-                                        const dollars = data.datasets[datasetIndex].data.currentIndex
-                                        const currencyTooltip = dollars.toLocaleString('en-US', { 
-                                            style: 'currency', 
-                                            currency: 'USD',
-                                            maximumFractionDigits: 2
-                                        })
-                                        return currencyTooltip
-                                    }
-                                }
-                            }}
-                        }
-                        maintainAspectRatio={false}
-                    />
+                <div 
+                    style={{width:"100%", position: "relative"}}
+                >
+                <Doughnut 
+                    data={DATA}
+                    options={
+                        createOptions(
+                            {
+                                min_width_1800px: mq6Ref.current,
+                                min_width_1400px: mq1Ref.current,
+                                min_width_1100px: mq2Ref.current,
+                                min_width_862px: mq5Ref.current,
+                                min_width_800px: mq3Ref.current,
+                                min_width_600px: mq4Ref.current
+                            }
+                        )
+                    }
+                    maintainAspectRatio={false}
+                />
                 </div>
             </div>
         </div>
-
-
     )
 }
 
