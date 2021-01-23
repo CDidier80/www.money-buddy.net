f56b263d5bb42a9280dccff7cbc3381da40560b9
Collin Didier <collin.didier80@gmail.com>
2021-01-23 11:34:04 -0600

    move subcomponents into their own file

--- client/src/pages/Dashboard/components/NavBar/NavBar.js
+++ client/src/pages/Dashboard/components/NavBar/NavBar.js
@@ -1,73 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { IconButton } from '@material-ui/core';
-import blueGrey from '@material-ui/core/colors/blueGrey';
+import React from 'react'
+import AccountPageLink from "./components/AccountPageLink"
+import LandingPageLink from "./components/LandingPageLink"
+import SignOutLink from "./components/SignOutLink"
+import Burger from "./components/Burger"
 
 const NavBar = (props) => {
 
-    { /* PROPS */}
-
-    const { 
-        narrow, 
-        setSidebarNarrowed,
-        ticker,
-        setTicker
-    } = props.fromDashboard
-
-
-
-    { /* FUNCTIONS */}
-    
-    const logOut = (e) => {
-        props.setAuth(false)
-        props.setUserInfo(null)
-        localStorage.clear()
-        props.history.push("/")
-    }
-
-    const handleBurger = (e) => {
-        e.preventDefault()
-        console.log("burger's narrow prop: ", narrow)
-        setSidebarNarrowed(narrow ? false : true) 
-        setTicker(ticker + 1)
-    }
-
     return (
         <nav className="dash-navbar" >
-            <Link to="/" className="title-link">
-                <span className="money">Money</span>
-                Buddy
-            </Link> 
-            <IconButton 
-                onClick={(e)=>handleBurger(e)} 
-                className="hamburger-iconbutton-wrapper" 
-                style={{margin: "6px 0 0 6px"}
-            }>
-                <MenuIcon 
-                    className="hamburger" 
-                    fontSize="default" 
-                    htmlColor={blueGrey[50]}
-                />
-            </IconButton> 
-
-            <Link 
-                className="sign-out-link" 
-                to="#"
-                onClick={()=>logOut()}
-            >
-                Sign Out
-            </Link>
-
-            <IconButton 
-                onClick={()=> props.history.push("/dashboard/account")}>
-                <AccountCircleIcon                     
-                    className="account-icon" 
-                    fontSize="default" 
-                    htmlColor={blueGrey[50]}
-                />
-            </IconButton> 
+            <LandingPageLink />
+            <Burger 
+                {...props}
+            />
+            <SignOutLink 
+                {...props}
+            />
+            <AccountPageLink 
+                {...props}
+            />
         </nav>
     )
 }
