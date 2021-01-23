efefbdee93a43864fb22300d41f796d757d07c5b
Collin Didier <collin.didier80@gmail.com>
2021-01-23 11:39:54 -0600

    modify tests

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,18 @@
-switch (true) {
-    case true:
-        console.log("big")
-    case false:
-        console.log("big")
-    case true:
-        console.log("big")
+bool1 = false 
+bool2 = true
+bool3 = true
 
+
+switch (true) {
+    case bool1:
+        console.log("1")
+        break
+    case bool2:
+        console.log("2")
+        break
+    case bool3:
+        console.log("3")
     default:
         (()=>null)()
 }
+
