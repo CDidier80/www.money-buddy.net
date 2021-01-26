0dd38ace5ca5f7fd6ed0f9d096b93c5badb2fb3b
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:44:35 -0600

    add 2nd version of currency function as callback

--- client/src/pages/Dashboard/sub-pages/universal-functions/cellFormatting.js
+++ client/src/pages/Dashboard/sub-pages/universal-functions/cellFormatting.js
@@ -1,4 +1,4 @@
-export const format = (value) => {
+export const currencyFormat = (value) => {
 
     let formattedValue = value.toLocaleString('en-US', { 
         style: 'currency', 
@@ -11,3 +11,18 @@ export const format = (value) => {
     }
     return formattedValue
 }
+
+export const currencyChartCallback = {
+    callback: (value) => {
+
+    let formattedValue = value.toLocaleString('en-US', { 
+        style: 'currency', 
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0, 
+    })
+    if (value < 0){
+        formattedValue = "-" + formattedValue
+    }
+    return formattedValue
+}}
