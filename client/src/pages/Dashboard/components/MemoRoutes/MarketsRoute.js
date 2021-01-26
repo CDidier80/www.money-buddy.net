bf7951c4dc8589545f192586653a85bc61c9b684
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:33:29 -0600

    new markets route for future page

---
+++ client/src/pages/Dashboard/components/MemoRoutes/MarketsRoute.js
@@ -0,0 +1,18 @@
+import React, { memo } from 'react';
+import { Route } from 'react-router-dom'
+import Markets from "../../sub-pages/Markets/Markets"
+
+const MarketsRoute = memo((props) => {
+    
+    return (
+        <Route 
+            component={ () => ( 
+                <Markets {...props}/> 
+        )} 
+    />
+    )
+},(prevProps, nextProps) => {
+    return (prevProps.ticker !== nextProps.ticker)
+})
+
+export default MarketsRoute
