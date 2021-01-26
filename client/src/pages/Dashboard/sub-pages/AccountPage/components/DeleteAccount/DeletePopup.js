7edec0853085acad480b3cd0eac1119954f73672
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:38:14 -0600

    new file location

---
+++ client/src/pages/Dashboard/sub-pages/AccountPage/components/DeleteAccount/DeletePopup.js
@@ -0,0 +1,45 @@
+import { makeStyles } from '@material-ui/styles';
+import React, { useState } from 'react';
+import { DeleteUser} from "../../../../../../Services/UserService"
+
+const DeletePopup = (props) => {
+
+    const {userId, history, setDeleteTriggered} = props
+
+
+
+    const deleteAccount = async (e) => {
+        e.preventDefault()
+        console.log(userId)
+        await DeleteUser({userId})
+        history.push("/")
+    }
+
+    return (
+        <div 
+            className="customBackdrop" 
+            onClick={(e) => setDeleteTriggered(false)} 
+        >
+            <div className="popup">
+                <p className="warning">Warning: This action is irreversible.</p>
+                <div className="button-container">
+                    <button 
+                        onClick={(e)=>deleteAccount(e)}
+                        className="submitButton delete-account-button"
+                    >
+                        Delete Account
+                    </button>
+                    <button 
+                        className="submitButton return"
+                        onClick={() => setDeleteTriggered(false)}
+                    >
+                        Return
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default DeletePopup
