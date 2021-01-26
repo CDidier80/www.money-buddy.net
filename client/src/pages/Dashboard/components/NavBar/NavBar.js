7e6c785c443c64c29c91fcc785a1b8ce59cbdccc
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:35:25 -0600

    temp screen dimensions added with event listener

--- client/src/pages/Dashboard/components/NavBar/NavBar.js
+++ client/src/pages/Dashboard/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import AccountPageLink from "./components/AccountPageLink"
 import LandingPageLink from "./components/LandingPageLink"
 import SignOutLink from "./components/SignOutLink"
@@ -6,12 +6,26 @@ import Burger from "./components/Burger"
 
 const NavBar = (props) => {
 
+
+    const [size, setSize] = useState(`${window.innerWidth} x ${window.innerHeight}`)
+
+    const reportSize = (e) => {
+        setSize(`${window.innerWidth} x ${window.innerHeight}`)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", reportSize)
+        return () => {
+            window.removeEventListener("resize", reportSize)
+        }
+    }, [])
     return (
-        <nav className="dash-navbar" >
+        <nav className="dash-navbar">
             <LandingPageLink />
             <Burger 
                 {...props}
             />
+            <h2 style={{color:"white", fontSize:"30px"}}>{size}</h2>
             <SignOutLink 
                 {...props}
             />
