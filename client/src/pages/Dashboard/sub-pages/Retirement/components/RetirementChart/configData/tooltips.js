4cb195fb91d56a9f80b1cbf309dd1afa3f61b7e2
Collin Didier <collin.didier80@gmail.com>
2021-01-26 02:34:01 -0600

    new modules for retirement chart config functions/data

---
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/configData/tooltips.js
@@ -0,0 +1,48 @@
+// for "Age xx" on tooltips
+const titleStyles = {
+    titleMarginBottom: 8,
+    titleAlign: "center",
+    titleFontSize: 14,
+}
+
+// for "$XXX" on tooltips
+const bodyStyles = {
+    bodyAlign: "center",
+    bodyFontSize: 14,
+}
+
+// for tooltip entity
+const tooltipStyles = {
+    backgroundColor: "#0a6666",
+    borderColor: "#ffd781",
+    displayColors: false,
+    caretPadding: 13,
+    cornerRadius: 6,
+    borderWidth: 2,
+    caretSize: 9,
+    yPadding: 11,
+    xPadding: 11,
+}
+
+export const tooltips = {
+    ...tooltipStyles,
+    ...titleStyles,
+    ...bodyStyles,
+    callbacks: {
+        label:  (tooltipItem, data) => {
+            const { datasetIndex } = tooltipItem
+            const currentIndex = tooltipItem.index
+            const dollars = data.datasets[datasetIndex].data[currentIndex]
+            const currencyTooltip = dollars.toLocaleString('en-US', { 
+                style: 'currency', 
+                currency: 'USD',
+                maximumFractionDigits: 0,
+                maximumFractionDigits: 0
+            })
+            return currencyTooltip
+        },
+        title: (tooltipItem, data) => {
+            console.log(tooltipItem)
+            return `Age: ${tooltipItem[0]["label"]}`
+    }}
+}
