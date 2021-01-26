c3e60213888a29b6192c91df031995b1955932f7
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:39:11 -0600

    new component for account page email submit button

---
+++ client/src/pages/Dashboard/sub-pages/AccountPage/components/EmailForm/components/EmailButton.js
@@ -0,0 +1,45 @@
+import { UpdateEmail } from "../../../../../../../Services/UserService"
+import React from 'react'
+
+const EmailButton = (props) => {
+
+    const {id: user_id, newEmail} = props
+
+    const {
+        errorSnackbar,
+        updateSnackbar,
+    } = props.fromAccountPage
+
+
+    
+    const submitNewEmail = async (e) => {
+        e.preventDefault()
+        // log(3)
+        if (newEmail === "") {
+            errorSnackbar("Email Address")
+            return
+        }
+        try {
+            const response = await UpdateEmail({userId: user_id, email: newEmail})
+            console.log("submit new email response: ", response)
+            if (response.status === 200) {
+                updateSnackbar('Email Address')
+            }
+        } catch (error) {
+            console.log(error)
+            errorSnackbar('Email Address')
+        }
+    }
+
+    return (
+        <button 
+            className="submitButton account-page"
+            onClick={(e)=>submitNewEmail(e)}
+        >
+            Submit
+        </button>
+    )
+}
+
+export default EmailButton
+
