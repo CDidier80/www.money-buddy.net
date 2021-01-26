2e51fa8eb4d9b503127f395aeb8d94a97cc6ec3e
Collin Didier <collin.didier80@gmail.com>
2021-01-26 02:33:22 -0600

    move functions to other files for import

--- client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/RetirementChart.js
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/RetirementChart.js
@@ -1,10 +1,20 @@
 import { calculateSavingsForecast } from "./components/functionModules/totalSavings"
-import { universalProperties, tooltips } from "./components/data"
+import { generateScales } from "./configData/axesAndTicks"
+import {generateDataSets} from "./configData/data"
+import { tooltips } from "./configData/tooltips"
 import LineChart from "./components/LineChart"
 import React from 'react'
-import { fiveYearTicks, makeXAxisAgeLabels } from "./components/functionModules/configFunctions"
+import { 
+    fiveYearTicks, 
+    makeXAxisAgeLabels 
+} from "./components/functionModules/configFunctions"
+
+
 
 const ChartLogic = (props) => {
+
+
+    /* ----------------------------- PROPS ----------------------------- */
 
     const { callback: toCurrency } = props.fromApp.currencyChartCallback
     const { lifeSpanAge, currentAge, endingAge } = props.fromRetirement
@@ -12,52 +22,15 @@ const ChartLogic = (props) => {
 
 
 
+    /* ---------------------- ASSEMBLE CHART PROPS ---------------------- */
     const chartData = {
         labels: makeXAxisAgeLabels(lifeSpanAge, currentAge, endingAge),
-        datasets: [
-            {
-                label: "", 
-                fill: true,
-                data: savingsForecast,
-                ...universalProperties,
-                borderColor: 'rgba(10,102,102,.7)',
-            }
-        ]
-    }
-
-    const axesStyle = {
-        fontFamily: "Roboto",
-        fontStyle: 'Bold',
-        fontColor: "black",
-        fontSize: 14,
-        padding: 5
+        datasets: generateDataSets(savingsForecast)
     }
 
     const options = {
-        legend: {
-            display: false
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    callback: (value) => toCurrency(value),
-                    ...axesStyle
-                },
-            }],
-            xAxes: [{
-                gridLines: {
-                    display: false
-                },
-                ticks: {
-                    autoSkip: false,
-                    callback: ( value, index, values ) => {
-                        return fiveYearTicks( value, index, values )
-                    },
-                    ...axesStyle,
-                    
-                },
-            }],
-        },
+        legend: { display: false },
+        scales: generateScales(toCurrency, fiveYearTicks),
         tooltips: tooltips
     }
 
