ed6f838425da9659731fa6fb71d802a410e670a6
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:33:48 -0600

    reformat

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import { Switch, Route, withRouter } from 'react-router-dom'
-import Dashboard from "./pages/Dashboard/Dashboard.js"
-import LandingPage from "./pages/LandingPage/LandingPage.js"
 import SignInSignUpPage from "./pages/SignInSignUp/SignInSignUpPage.js"
+import { Switch, Route, withRouter } from 'react-router-dom'
+import LandingPage from "./pages/LandingPage/LandingPage.js"
 import { CheckSessionService } from './Services/UserService'
+import { gradientWrapper } from "./modules/styles"
+import { currencyFormat, currencyChartCallback } from "./modules/clientFunctions"
+import Dashboard from "./pages/Dashboard/Dashboard.js"
 import { SnackbarProvider } from 'notistack';
+import React, { useState } from 'react'
 
 
 const App = (props) => {
-    const [pageIsLoaded, setLoaded] = useState(true)
-    const [session, setSession] = useState(null)
-    const [authenticated, setAuth] = useState(false)
-    const [userInfo, setUserInfo]= useState({})
+  const [userInfo, setUserInfo]= useState({})
+  const [session, setSession] = useState(null)
+  const [pageIsLoaded, setLoaded] = useState(true)
+  const [authenticated, setAuth] = useState(false)
 
 
     const verifyTokenValid = async () => {
@@ -30,53 +32,61 @@ const App = (props) => {
       }
   }
 
-    const propsForRoutes = {
-      authenticated, 
-      userInfo,
-      setAuth,
-      setUserInfo
-    }
 
-    // console.log("App.js RENDERED. CURRENT AUTH: ", authenticated)
-    return (
-      <SnackbarProvider 
-        maxSnack={3} 
-        style={{fontWeight: "bold"}
-      }>
-          <main className="app">
-            {!pageIsLoaded ? <div><h3>Loading...</h3></div> : 
-              <Switch> 
-                  <Route exact path="/"      
-                      component={ (props) => (
-                          <LandingPage 
-                              {...props} 
-                              fromApp={{...propsForRoutes}}
-                          />
-                      )}
-                  />
-                  <Route path="/login"        
-                      component={ (props) => ( 
-                          <SignInSignUpPage         
-                              {...props}  
-                              fromApp={{...propsForRoutes}}
-                              
-                          />
-                      )}
-                  />
-                  <Route path="/dashboard"        
-                      component={ (props) => ( 
-                          <Dashboard         
-                              {...props}  
-                              fromApp={{...propsForRoutes}}
-                          />
-                      )}
-                  />  
-              </Switch>
-            }
-          </main>
-      </SnackbarProvider>
-    )
+
+  const propsForRoutes = {
+    setAuth,
+    userInfo,
+    setUserInfo,
+    authenticated, 
+    currencyFormat,
+    gradientWrapper,
+    currencyChartCallback
   }
+
+  // console.log("App.js RENDERED. CURRENT AUTH: ", authenticated)
+  return (
+    <SnackbarProvider 
+      maxSnack={3} 
+      style={{fontWeight: "bold"}
+    }>
+        <main className="app">
+          {!pageIsLoaded ? <div><h3>Loading...</h3></div> : 
+            <Switch> 
+                <Route 
+                    exact path="/"      
+                    component={ (props) => (
+                        <LandingPage 
+                            {...props} 
+                            fromApp={{...propsForRoutes}}
+                        />
+                    )}
+                />
+                <Route 
+                    path="/login"        
+                    component={ (props) => ( 
+                        <SignInSignUpPage         
+                            {...props}  
+                            fromApp={{...propsForRoutes}}
+                            
+                        />
+                    )}
+                />
+                <Route 
+                    path="/dashboard"        
+                    component={ (props) => ( 
+                        <Dashboard         
+                            {...props}  
+                            fromApp={{...propsForRoutes}}
+                        />
+                    )}
+                />  
+            </Switch>
+          }
+        </main>
+    </SnackbarProvider>
+  )
+}
 
 export default withRouter(App)
 
