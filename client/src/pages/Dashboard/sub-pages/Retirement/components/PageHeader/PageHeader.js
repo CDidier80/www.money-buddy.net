993a37eb39980e92dd47e6d92abc2d8e52b06530
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:48:09 -0600

    retirement page header component

---
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/PageHeader/PageHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageHeader = () => {
+    return (
+        <div className="header-and-button-wrapper" style={{marginBottom: "20px"}}>
+            <div className="page-header-wrapper">
+                <h1 className="page-header">RETIREMENT</h1>
+            </div>
+        </div>
+    )
+}
+
+export default PageHeader
