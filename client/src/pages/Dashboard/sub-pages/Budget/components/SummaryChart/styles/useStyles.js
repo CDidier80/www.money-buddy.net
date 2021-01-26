82aeb68afa7713999e8e518e1080cfb4a5459703
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:51:14 -0600

    del unused styles

--- client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/styles/useStyles.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/styles/useStyles.js
@@ -1,12 +1,6 @@
 import { makeStyles, } from '@material-ui/core'
 
 
-const buttons = {
-    fontSize: "9px",
-    fontWeight: "700",
-    fontFamily: "Lato, sans-serif",
-    padding: "0 5px 0 5px"
-}
 
 const useStyles = makeStyles({
     title: {
@@ -15,22 +9,6 @@ const useStyles = makeStyles({
     tableContainer: {
         maxWidth: "890px",
         margin: "auto"
-    },
-    buttonGroup: {
-        height: "30px",
-        width: "50%",
-        display: "block",
-        margin: "10px auto 0 auto",
-        textAlign: "center"
-    },
-    activeButton: {
-        ...buttons,
-        color: "#22c1c3",
-
-    },
-    inactiveButton: {
-        ...buttons,
-        color: "lightgray",
     },
 })
 
