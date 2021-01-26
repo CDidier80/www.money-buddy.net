e7c9809b8220b4ee0fe20695a14672171e2c5605
Collin Didier <collin.didier80@gmail.com>
2021-01-26 01:52:55 -0600

    reformat and change style data

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/Chart/chartData.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/Chart/chartData.js
@@ -14,21 +14,21 @@ export const labels = [
 
 
 export const universalProperties = {
-    lineTension: 0.1,
-    borderCapStyle: 'butt',
-    backgroundColor: 'rgba(40,211,236, 0.2)',
+    pointRadius: 1,
     borderDash: [],
-    borderDashOffset: 0.0,
-    borderJoinStyle: 'miter',
-    pointBorderColor: 'rgba(75,192,192,1)',
-    pointBackgroundColor: '#fff',
+    lineTension: 0.1,
+    pointHitRadius: 10,
     pointBorderWidth: 1,
     pointHoverRadius: 5,
-    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    borderDashOffset: 0.0,
+    borderCapStyle: 'butt',
+    borderJoinStyle: 'miter',
     pointHoverBorderWidth: 2,
-    pointRadius: 1,
-    pointHitRadius: 10,
+    pointBackgroundColor: '#fff',
+    pointBorderColor: 'rgba(75,192,192,1)',
+    backgroundColor: 'rgba(40,211,236, 0.2)',
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
 }
 
 export const tooltips = {
@@ -48,3 +48,24 @@ export const tooltips = {
         }
     }
 }
+
+
+const axesStyle = {
+    fontFamily: "Roboto",
+    fontStyle: 'Bold',
+    fontColor: "black",
+    fontSize: 13,
+    padding: 5
+}
+
+export const configureAxes = (toCurrency) => ({
+    yAxes: [{
+        ticks: { 
+            callback: (value) => toCurrency(value),
+            ...axesStyle,
+        },
+    }],
+    xAxes: [{
+        ticks: { ...axesStyle },
+    }],
+})
