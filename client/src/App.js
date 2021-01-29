1d81dc45d54ad0157ea129e8550a33df3a2062d9
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:46:41 -0600

    move/delete/reformat

--- client/src/App.js
+++ client/src/App.js
@@ -1,91 +1,105 @@
-import SignInSignUpPage from "./pages/SignInSignUp/SignInSignUpPage.js"
-import { Switch, Route, withRouter } from 'react-router-dom'
-import LandingPage from "./pages/LandingPage/LandingPage.js"
-import { CheckSessionService } from './Services/UserService'
-import { gradientWrapper } from "./modules/styles"
-import { currencyFormat, currencyChartCallback } from "./modules/clientFunctions"
-import Dashboard from "./pages/Dashboard/Dashboard.js"
-import { SnackbarProvider } from 'notistack';
-import React, { useState } from 'react'
+import { 
+    Route, 
+    Switch, 
+    withRouter 
+} from 'react-router-dom'
+import { 
+    currencyFormat, 
+    currencyChartCallback 
+} from "./modules/clientFunctions"
+import  React, { useState }        from  'react'
+import  { SnackbarProvider }       from  'notistack';
+import  { gradientWrapper }        from  "./modules/styles"
+import  { CheckSessionService }    from  './Services/UserService'
+import  Dashboard                  from  "./pages/Dashboard/Dashboard.js"
+import  LandingPage                from  "./pages/LandingPage/LandingPage.js"
+import  SignInSignUpPage           from  "./pages/SignInSignUp/SignInSignUpPage.js"
+
 
 
 const App = (props) => {
-  const [userInfo, setUserInfo]= useState({})
-  const [session, setSession] = useState(null)
-  const [pageIsLoaded, setLoaded] = useState(true)
-  const [authenticated, setAuth] = useState(false)
 
+    /* ------------------- STATE ----------------------*/
+
+    const [userInfo, setUserInfo]= useState({})
+    const [session, setSession] = useState(null)
+    const [pageIsLoaded, setLoaded] = useState(true)
+    const [authenticated, setAuth] = useState(false)
+
+
+  /* -------------------- FUNCTIONS---------------------*/
 
     const verifyTokenValid = async () => {
-      const token = localStorage.getItem('token')
-      if (token) {
-        try {
-            const session = await CheckSessionService()
-            setSession(session)
-            setAuth(true)
-            (() => props.history.push('/main'))()
+        const token = localStorage.getItem('token')
+        if (token) {
+            try {
+                const session = await CheckSessionService()
+                setSession(session)
+                setAuth(true)
+                (() => props.history.push('/main'))()
             } catch (error) {
-              setSession(null)
-              setAuth(false)
-              localStorage.clear()
+                setAuth(false)
+                setSession(null)
+                localStorage.clear()
             }
-      }
-  }
+        }
+    }
 
+    /* --------------- PROPS FOR CHILDREN ---------------*/
 
+    const propsForRoutes = {
+        setAuth,
+        userInfo,
+        setUserInfo,
+        authenticated, 
+        currencyFormat,
+        gradientWrapper,
+        currencyChartCallback
+    }
 
-  const propsForRoutes = {
-    setAuth,
-    userInfo,
-    setUserInfo,
-    authenticated, 
-    currencyFormat,
-    gradientWrapper,
-    currencyChartCallback
-  }
+    /* ---------------------- JSX ----------------------*/
 
-  // console.log("App.js RENDERED. CURRENT AUTH: ", authenticated)
-  return (
-    <SnackbarProvider 
-      maxSnack={3} 
-      style={{fontWeight: "bold"}
-    }>
-        <main className="app">
-          {!pageIsLoaded ? <div><h3>Loading...</h3></div> : 
-            <Switch> 
-                <Route 
-                    exact path="/"      
-                    component={ (props) => (
-                        <LandingPage 
-                            {...props} 
-                            fromApp={{...propsForRoutes}}
+    return (
+        <SnackbarProvider 
+            maxSnack={3} 
+            style={{fontWeight: "bold"}
+        }>
+            <main className="app">
+                {!pageIsLoaded ? <div></div> : 
+                    <Switch> 
+                        <Route 
+                            exact path="/"      
+                            component={ (props) => (
+                                <LandingPage 
+                                    {...props} 
+                                    fromApp={{...propsForRoutes}}
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Route 
-                    path="/login"        
-                    component={ (props) => ( 
-                        <SignInSignUpPage         
-                            {...props}  
-                            fromApp={{...propsForRoutes}}
-                            
+                        <Route 
+                            path="/login"        
+                            component={ (props) => ( 
+                                <SignInSignUpPage         
+                                    {...props}  
+                                    fromApp={{...propsForRoutes}}
+                                    
+                                />
+                            )}
                         />
-                    )}
-                />
-                <Route 
-                    path="/dashboard"        
-                    component={ (props) => ( 
-                        <Dashboard         
-                            {...props}  
-                            fromApp={{...propsForRoutes}}
-                        />
-                    )}
-                />  
-            </Switch>
-          }
-        </main>
-    </SnackbarProvider>
-  )
+                        <Route 
+                            path="/dashboard"        
+                            component={ (props) => ( 
+                                <Dashboard         
+                                    {...props}  
+                                    fromApp={{...propsForRoutes}}
+                                />
+                            )}
+                        />  
+                    </Switch>
+                }
+            </main>
+        </SnackbarProvider>
+    )
 }
 
 export default withRouter(App)
--- client/src/modules/styles.js
+++
@@ -1,9 +0,0 @@
-export const gradientWrapper = {
-    borderRadius: "8px",
-    backgroundColor: "#074141",
-    boxShadow:`        
-        2px 3px 1px -2px rgba(0,0,0,0.3),
-        2px 2px 2px 0px  rgba(0,0,0,0.22),
-        2px 1px 8px 0px rgba(0,0,0,0.18);`,
-    backgroundImage: "linear-gradient(180deg, #0a6666, #a0720e)"
-}
