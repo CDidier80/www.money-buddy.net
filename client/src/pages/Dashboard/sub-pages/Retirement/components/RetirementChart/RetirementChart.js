ff5c8280a906703237365e90a4efca8feea98318
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:55:40 -0600

    dynamic line chart for retirement page

--- client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/RetirementChart.js
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/RetirementChart.js
@@ -1,41 +1,66 @@
+import { fiveYearTicks, makeXAxisAgeLabels } from "./components/functionModules/configFunctions"
 import { calculateSavingsForecast } from "./components/functionModules/totalSavings"
 import { generateScales } from "./configData/axesAndTicks"
+import React, { useState, useEffect } from 'react'
 import {generateDataSets} from "./configData/data"
 import { tooltips } from "./configData/tooltips"
 import LineChart from "./components/LineChart"
-import React from 'react'
-import { 
-    fiveYearTicks, 
-    makeXAxisAgeLabels 
-} from "./components/functionModules/configFunctions"
-
 
 
 const ChartLogic = (props) => {
 
+    /* -------------------------------- PROPS -------------------------------- */
 
-    /* ----------------------------- PROPS ----------------------------- */
-
-    const { callback: toCurrency } = props.fromApp.currencyChartCallback
-    const { lifeSpanAge, currentAge, endingAge } = props.fromRetirement
     const savingsForecast = calculateSavingsForecast({...props.fromRetirement})
+    const { callback: toCurrency } = props.fromApp.currencyChartCallback
+    const { 
+        lifespanAge, 
+        currentAge, 
+        endingAge 
+    } = props.fromRetirement
 
 
+    /* ------------------------- Listen to Window Size ------------------------ */
+
+    const [height, setHeight] = useState(window.innerHeight / 2)
+    const [width, setWidth] = useState(window.innerWidth - 200)
+
+    const setChartWrapperSize = (e) => {
+        setHeight(window.innerHeight / 2)
+        setWidth(window.innerWidth - 200)
+    }
+    
+    useEffect(() => {
+        window.addEventListener("resize", setChartWrapperSize)
+        return () => window.removeEventListener("resize", setChartWrapperSize)
+    }, [])
+    
 
     /* ---------------------- ASSEMBLE CHART PROPS ---------------------- */
+
     const chartData = {
-        labels: makeXAxisAgeLabels(lifeSpanAge, currentAge, endingAge),
+        labels: makeXAxisAgeLabels(lifespanAge, currentAge, endingAge),
         datasets: generateDataSets(savingsForecast)
     }
 
     const options = {
-        legend: { display: false },
         scales: generateScales(toCurrency, fiveYearTicks),
-        tooltips: tooltips
+        maintainAspectRatio: false,
+        legend: { display: false },
+        tooltips: tooltips,
+        responsive: true,
     }
 
+    /* ------------------ JSX ------------------ */
+
     return (
-        <div>
+        <div 
+            className="retirement-chart-wrapper"
+            style={{
+                width: width,
+                height: height
+            }}
+        >
             <LineChart 
                 data={chartData}
                 options={options}
