5920096012b1370d2a891f4fdedf52be799ea18d
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:47:49 -0600

    add deep clone prop check function and dollar formatting function

---
+++ client/src/pages/modules/clientFunctions.js
@@ -0,0 +1,54 @@
+export const currencyFormat = (value) => {
+
+    let formattedValue = value.toLocaleString('en-US', { 
+        style: 'currency', 
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0, 
+    })
+    if (value < 0){
+        formattedValue = "-" + formattedValue
+    }
+    return formattedValue
+}
+
+export const currencyChartCallback = {
+    callback: (value) => {
+        let formattedValue = value.toLocaleString('en-US', { 
+            style: 'currency', 
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0, 
+        })
+        if (value < 0){
+            formattedValue = "-" + formattedValue
+        }
+        return formattedValue
+    }
+}
+
+export const makeDeepClone = (object) => JSON.parse(JSON.stringify(object))
+
+
+export const stringifyProps = (props) => {
+    const propsOfTypeNumber = Object.values(props).filter(value => typeof value === "number")
+    const numArrayToSingleString = propsOfTypeNumber.toString()
+    return numArrayToSingleString
+}
+
+
+export const deepPropCheck = (prevProps, nextProps) => {
+    const propsAreEqual = stringifyProps(prevProps) === stringifyProps(nextProps)
+    const shouldRefuseRerender =  propsAreEqual ? true : false
+    return shouldRefuseRerender
+}
+
+
+export const dollarFormat = (value, i) => {
+    if (String(value).length < 4) {
+        return `$${value}`
+    } else {
+        let valInThousands = Math.round(value / 1000)
+        return `$${valInThousands}k`
+    }
+}
