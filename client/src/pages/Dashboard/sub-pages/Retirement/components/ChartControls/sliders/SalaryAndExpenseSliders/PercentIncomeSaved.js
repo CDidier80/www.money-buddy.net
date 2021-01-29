b7bf2a137b9014f691712ddf6dfa7db4b10f51f7
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:52:47 -0600

    add slider component for % income saved

---
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/ChartControls/sliders/SalaryAndExpenseSliders/PercentIncomeSaved.js
@@ -0,0 +1,29 @@
+import ThemedSlider from "../ThemedSlider"
+import { deepPropCheck } from "../../../../../../../../modules/clientFunctions"
+import React, { memo } from 'react'
+
+const PercentIncomeSaved = memo((props) => {
+
+    const { 
+        makeMarks, 
+        percentIncomeSaved, 
+        setPercentIncomeSaved, 
+    } = props
+
+    const themedSliderProps = {
+        marks: makeMarks(0, 100),
+        min: 0, step: 1, max: 100,
+        hookValue: percentIncomeSaved,
+        ariaLabel: "perc-income-saved-slider",
+        makeLabel: (value, index) => `${value}%`,
+        setHook: (e, value) => setPercentIncomeSaved(value),
+    }
+
+    return (
+        <ThemedSlider fromSliderParent={{...themedSliderProps}} />
+    )
+}, 
+    (prevProps, nextProps) => deepPropCheck(prevProps, nextProps)
+)
+
+export default PercentIncomeSaved
