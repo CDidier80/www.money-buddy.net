79f9b64df85f372fa5ee953a0a87b3209e63a7c0
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:53:42 -0600

    add slider component for salary

---
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/ChartControls/sliders/SalaryAndExpenseSliders/Salary.js
@@ -0,0 +1,33 @@
+import { deepPropCheck } from "../../../../../../../../modules/clientFunctions"
+import ThemedSlider from "../ThemedSlider"
+import React, { memo } from 'react'
+
+
+const Salary = memo((props) => {
+    /* --------- setState hook for this slider --------- */
+    const { 
+        salary, 
+        makeMarks,
+        setSalary, 
+        dollarFormat, 
+    } = props
+
+    const themedSliderProps = {
+        hookValue: salary,
+        ariaLabel: "salary-slider",
+        min: 0, step: 500, max: 500000,
+        setHook: (e, value) => setSalary(value),
+        marks: makeMarks(0, 500000,),
+        makeLabel: (v, i ) => dollarFormat(v, i),
+    }
+
+    return (
+        <ThemedSlider
+            fromSliderParent={{...themedSliderProps}}
+        />
+    )
+}, 
+    (prevProps, nextProps) => deepPropCheck(prevProps, nextProps)
+)
+
+export default Salary
