43fb69427bf461fda5d1222bfba23eb07c766dab
Collin Didier <collin.didier80@gmail.com>
2021-01-29 01:49:14 -0600

    reformat,fix prop name, del comments

--- client/src/pages/Dashboard/sub-pages/Retirement/Retirement.js
+++ client/src/pages/Dashboard/sub-pages/Retirement/Retirement.js
@@ -1,29 +1,57 @@
-import RetirementChart from "./components/RetirementChart/RetirementChart"
-import ChartControls from "./components/ChartControls/ChartControls"
-import PageHeader from "./components/PageHeader/PageHeader"
-import React, {useState, useEffect} from 'react'
+import RetirementChart     from  "./components/RetirementChart/RetirementChart"
+import StateUpdateTest     from  "./components/ChartControls/StateUpdateTest"
+import ChartControls       from  "./components/ChartControls/ChartControls"
+import PageHeader          from  "./components/PageHeader/PageHeader"
+import React, { 
+    useState, 
+    useEffect 
+} from  "react"
 import "./styles/retirement.css"
 
 const Retirement = (props) => {
 
+    /* --------------------------- STATE --------------------------- */
+
+    /* --- age --- */
     const [endingAge, setEndingAge] = useState(65)
     const [currentAge, setCurrentAge] = useState(30)
-    const [lifeSpanAge, setLifespanAge] = useState(78)
+    const [lifespanAge, setLifespanAge] = useState(78)
 
-    const [salary, setSalary] = useState(50000)
+    /* --- savings --- */
     const [savingsROI, setROISavings] = useState(5)
-    const [currentSavings, setCurrentSavings] = useState(0)
-    const [salaryGrowthRate, setSalaryGrowthRate] = useState(2)
+    const [currentSavings, setCurrentSavings] = useState(500)
     const [percentIncomeSaved, setPercentIncomeSaved] = useState(20)
 
+    /* --- salary/expenses --- */
+    const [salary, setSalary] = useState(50000)
+    const [salaryGrowthRate, setSalaryGrowthRate] = useState(2)
     const [retirementSpending, setRetirementSpending] = useState(50000)
+
+
+    /* ------------------------- Listen to Window Size ------------------------ */
+
+    // const [pageHeight, setPageHeight] = useState(window.innerHeight)
+    // const [pageWidth, setPageWidth] = useState(window.innerWidth)
+
+    // const setWindowSize = (e) => {
+    //     setPageHeight(window.innerHeight)
+    //     setPageWidth(window.innerWidth)
+    // }
+    
+    // useEffect(() => {
+    //     window.addEventListener("resize", setWindowSize)
+    //     return () => window.removeEventListener("resize", setWindowSize)
+    // }, [])
+    
+
+    /* ---------------------- PROPS FOR CHILDREN --------------------- */
 
     const hookValues = {
         salary, 
         endingAge, 
         savingsROI, 
         currentAge, 
-        lifeSpanAge, 
+        lifespanAge, 
         currentSavings, 
         salaryGrowthRate, 
         percentIncomeSaved, 
@@ -42,6 +70,10 @@ const Retirement = (props) => {
         setRetirementSpending,
     }
 
+    // const sizes = {
+    //     pageWidth,
+    //     pageHeight,
+    // }
 
 
     return (
@@ -50,10 +82,12 @@ const Retirement = (props) => {
             <RetirementChart 
                 {...props}
                 fromRetirement={{...hookValues}}
+                // sizes={{...sizes}}
             />
-            {/* <ChartControls 
-                fromRetirement={{...hookSetters}}
-            /> */}
+            <ChartControls 
+                {...props}
+                fromRetirement={{...hookSetters, ...hookValues}}
+            />
             
         </div>
     )
