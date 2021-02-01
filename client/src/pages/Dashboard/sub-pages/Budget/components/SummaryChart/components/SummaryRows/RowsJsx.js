2a18cd0585c62d8c76fb01f45823bd8e1144e9b8
Collin Didier <collin.didier80@gmail.com>
2021-02-01 14:52:00 -0600

    add colors to rows

--- client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/components/SummaryRows/RowsJsx.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/components/SummaryRows/RowsJsx.js
@@ -2,17 +2,22 @@ import SummaryCellPair from "./SummaryCellPair"
 import { TableRow } from '@material-ui/core'
 import React from 'react'
 
-const RowsJsx = ({data}) => (
-    <>
-        {data.map((cellPairProps, index) => (
-            <TableRow key={`${index}SummaryCells`}>
-                <SummaryCellPair 
-                    {...cellPairProps}
-                />
-            </TableRow>
-        ))}
-    </>
-)
+const RowsJsx = props => {
+    const { data, rowColors } = props
+    return(
+        <>
+            {data.map((cellPairProps, index) => (
+                <TableRow 
+                    key={`${index}SummaryCells`}
+                    className={rowColors[index]}
+                >
+                    <SummaryCellPair 
+                        {...cellPairProps}
+                    />
+                </TableRow>
+            ))}
+        </>
+)}
 
 
 export default RowsJsx
