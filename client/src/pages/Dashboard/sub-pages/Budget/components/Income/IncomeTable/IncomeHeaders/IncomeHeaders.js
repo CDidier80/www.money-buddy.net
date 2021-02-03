c97d81cd99bf3671cc4e8ac1770d56124e7a9cec
Collin Didier <collin.didier80@gmail.com>
2021-02-03 00:44:30 -0600

    add repsonsive styles

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/IncomeHeaders/IncomeHeaders.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/IncomeHeaders/IncomeHeaders.js
@@ -1,31 +1,29 @@
+import { TableCell, TableRow, makeStyles, useMediaQuery } from '@material-ui/core';
+import CellButtonGroup from "./buttons/CellButtonGroup"
 import React from 'react';
 
-import { 
-    TableCell, 
-    TableRow,
-    makeStyles, 
-    TableHead
-} from '@material-ui/core';
-
-
-const useStyles = makeStyles({
-
-    columnHeader: {
-        color: "#2c7b71",
-        fontWeight: "bold"
-    },
-    emptyCell: {
-        maxWidth: "36px",
-        minWidth: "36px", 
-        padding: "0px"
-    }
-})
 
 
 const IncomeHeaders = (props) => {
 
-    const classes = useStyles()
+    const smallerWidth = props.onlyTwoCells ? {width: "70x"} : {}
+    
+    const useStyles = makeStyles({
+    
+        columnHeader: {
+            color: "#2c7b71",
+            fontWeight: "bold",
+            ...props.textSize,
+        },
+        emptyCell: {
+            maxWidth: "36px",
+            minWidth: "36px", 
+            padding: "0px"
+        }
+    })
 
+    const classes = useStyles()
+    
     return (
         // <TableHead>
             <TableRow>
@@ -34,21 +32,28 @@ const IncomeHeaders = (props) => {
                 </TableCell>
                 <TableCell 
                     className={classes.columnHeader}
+                    style={smallerWidth}
                 >
                     Income Source
                 </TableCell>
-                <TableCell 
-                    className={classes.columnHeader} 
-                    align="right"
-                >
-                    Monthly Average
-                </TableCell>
-                <TableCell 
-                    className={classes.columnHeader} 
-                    align="right"
-                >
-                        Annual
-                </TableCell>
+                {props.onlyTwoCells ? 
+                    <CellButtonGroup {...props} />
+                :
+                    <>
+                        <TableCell 
+                            className={classes.columnHeader} 
+                            align="right"
+                        >
+                            Monthly Average
+                        </TableCell>
+                        <TableCell 
+                            className={classes.columnHeader} 
+                            align="right"
+                        >
+                                Annual
+                        </TableCell>
+                    </>
+                }
             </TableRow>
     )
 }
