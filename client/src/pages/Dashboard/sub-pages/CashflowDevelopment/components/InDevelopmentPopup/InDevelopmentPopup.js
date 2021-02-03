d74d7d37fc8086174be79def83e23dfb1723b55f
Collin Didier <collin.didier80@gmail.com>
2021-02-03 00:40:11 -0600

    add modal for display on cashflow page

---
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/InDevelopmentPopup/InDevelopmentPopup.js
@@ -0,0 +1,19 @@
+import ViewPageButton from './ViewPageButton'
+import DialogWrapper from "./DialogWrapper"
+import PopupHeader from "./PopupHeader"
+import Message from "./Message"
+import React from 'react'
+
+
+const InDevelopmentPopup = (props) => {
+
+    return (
+            <DialogWrapper>
+                <PopupHeader />
+                <Message />
+                <ViewPageButton {...props} />
+            </DialogWrapper>
+    )
+}
+
+export default InDevelopmentPopup
