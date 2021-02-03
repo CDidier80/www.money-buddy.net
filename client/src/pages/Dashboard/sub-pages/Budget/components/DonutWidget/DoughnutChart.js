90cd7835917c00b89643fe6721748ec37ce5e9fe
Collin Didier <collin.didier80@gmail.com>
2021-02-03 00:34:16 -0600

    finally fix chart resize responsiveness

--- client/src/pages/Dashboard/sub-pages/Budget/components/DonutWidget/DoughnutChart.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/DonutWidget/DoughnutChart.js
@@ -1,28 +1,30 @@
 import { createData, createOptions } from "./modules/donutFunctions"
 import { useMediaQuery } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2'
-import "./modules/useResponsiveStyle"
 import "./styles/donutStyles.css"
 import React, { 
-    useLayoutEffect, 
     useEffect, 
     useState, 
     useRef 
 } from 'react'
 
 
-
-
 const DoughnutChart = (props) => {
 
+    /* ---------------------------- PROPS ------------------------------ */
+    // console.log(props)
+    const { categoryNames, categoryTotals} = props.fromBudget
+    const { subpageRef, budgetRef } = props
     /* ---------------------- init MEDIA QUERIES ----------------------- */
-
+    
+    const mq6 = useMediaQuery('(min-width:1800px)')
     const mq1 = useMediaQuery('(min-width:1400px)')
     const mq2 = useMediaQuery('(min-width:1100px)')
-    const mq5 = useMediaQuery('(min-width:862px)' )
+    const mq5 = useMediaQuery('(min-width:887px)' )
     const mq3 = useMediaQuery('(min-width:800px)' )
-    const mq4 = useMediaQuery('(min-width:600px)' )
-    const mq6 = useMediaQuery('(min-width:1800px)')
+    const mq4 = useMediaQuery('(min-width:622px)' )
+    const mq7 = useMediaQuery('(min-width:515px)' )
+    // const mq7 = useMediaQuery('(min-width:515px)' )
 
     const mq1Ref = useRef("")
     const mq2Ref = useRef("")
@@ -30,6 +32,7 @@ const DoughnutChart = (props) => {
     const mq4Ref = useRef("")
     const mq5Ref = useRef("")
     const mq6Ref = useRef("")
+    const mq7Ref = useRef("")
 
     useEffect(() => {
         mq1Ref.current = mq1
@@ -38,30 +41,35 @@ const DoughnutChart = (props) => {
         mq4Ref.current = mq4
         mq5Ref.current = mq5
         mq6Ref.current = mq6
+        mq7Ref.current = mq7
     })
+    
+    const headerRef = useRef()
+    
+    
+    /* ---------------------------- STATE ------------------------------ */
+
+    const [useSubpageRef, setUseSubpageRef] = useState(window.innerWidth <= 650)
+    const [budgetWidth, setBudgetWidth] = useState("")
+    const [monthly, setMonthly] = useState(false)
+    const [width, setWidth] = useState(0)
 
     /* ------------------------- Listen to Window Size ------------------------ */
 
-    // const [height, setHeight] = useState(window.innerHeight / 2)
-    // const [width, setWidth] = useState(window.innerWidth - 200)
+    const handleDimensions = (e) => {
+        const oneColumn = window.innerWidth <= 650
+        const { clientWidth: headWidth } = headerRef.current
+        const { clientWidth: bWidth } = budgetRef.current
+        setWidth(headWidth)
+        setUseSubpageRef(oneColumn)
+        setBudgetWidth(bWidth)
+    }
 
-    // const setChartWrapperSize = (e) => {
-    //     setHeight(window.innerHeight / 2)
-    //     setWidth(window.innerWidth - 200)
-    // }
-    
-    // useLayoutEffect(() => {
-    //     window.addEventListener("resize", setChartWrapperSize)
-    //     return () => window.removeEventListener("resize", setChartWrapperSize)
-    // }, [])
-    /* ---------------------------- PROPS ------------------------------ */
-
-    const { categoryNames, categoryTotals} = props.fromBudget
-
-
-    /* ---------------------------- STATE ------------------------------ */
-
-    const [monthly, setMonthly] = useState(false)
+    useEffect(() => {
+        window.addEventListener("resize", handleDimensions)
+        handleDimensions()
+        return () => window.removeEventListener("resize", handleDimensions)
+    }, [])
 
     const DATA = createData(categoryNames, categoryTotals, monthly)
 
@@ -69,22 +77,28 @@ const DoughnutChart = (props) => {
         min_width_1800px: mq6Ref.current,
         min_width_1400px: mq1Ref.current,
         min_width_1100px: mq2Ref.current,
-        min_width_862px:  mq5Ref.current,
+        min_width_887px:  mq5Ref.current,
         min_width_800px:  mq3Ref.current,
-        min_width_600px:  mq4Ref.current
+        min_width_622px:  mq4Ref.current,
+        min_width_515px:  mq7Ref.current
     }
+
 
     return ( 
         <div 
             className="gradient-wrapper donut"
+            style={useSubpageRef ? {width: budgetWidth} : null}
         >
-            <div 
-                className="budget-top-widgets donut"
-            >
-                <h3 className="donut-header">
+            <div className="budget-top-widgets" >
+                <h3 
+                    className="donut-header" 
+                    ref={headerRef}
+                >
                     Distribution of Spending
                 </h3>
-                <div>
+                <div 
+                    style={{width: width}}
+                >
                     <Doughnut 
                         data={DATA}
                         maintainAspectRatio={false}
