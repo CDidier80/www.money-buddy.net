9ffd8d4e5fc49b487f2cefaa5523261866cc958a
Collin Didier <collin.didier80@gmail.com>
2021-02-04 17:13:05 -0600

    remove console logs

--- client/src/Services/ApiClient.js
+++ client/src/Services/ApiClient.js
@@ -5,14 +5,22 @@ const url = process.env.NODE_ENV === 'production' ?  `${window.location.origin}/
 
 const ApiClient = axios.create({ baseURL: url }) 
 
-// used only after user is logged in and authenticated. They maintain authenticated status by checking the validity of the current tokenbuggi
+// used only after user is logged in and authenticated. 
+// They maintain authenticated status by checking the validity of the current token
 ApiClient.interceptors.request.use(
     async (config) => {
     const token = localStorage.getItem('token')
     if (token) config.headers.Authorization = `Bearer ${token}`
     return config
     },
-    (err) => Promise.reject(err)
-)
+    (err) => {
+        // console.log("interceptors found auth failed")
+        return Promise.reject(err)
+})
 
 export default ApiClient
+
+
+    // console.log("interceptors fired")
+    // console.log("token plucked from local storage: ", token)
+    // console.log("config being returned:", config)
