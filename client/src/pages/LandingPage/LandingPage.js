8742c728175101d5f3e23c06a3a87269c48c30bc
Collin Didier <collin.didier80@gmail.com>
2021-02-03 21:50:14 -0600

    move jsx into smaller components

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,45 +1,15 @@
-import React from "react";
-import LandingNavBar from "./components/LandingNavBar"
-import MobileNavBar from "./components/MobileNavBar"
-import { CheckSessionService } from "../../Services/UserService";
+import ShapesContainer from "./components/ShapesContainer"
+import LandingNavBar from "./components/Navbar/LandingNavBar"
+import MobileNavBar from "./components/Navbar/MobileNavBar"
+import Hero from "./components/Hero"
 import "./styles/landingPage.css"
-import "./styles/hero.css"
 import "./styles/shapes.css"
 import "./styles/navbar.css"
+import "./styles/hero.css"
+import React from "react"
 
 
 const LandingPage = (props) => {
-
-    const floatyContainer = {
-        position: "absolute", 
-        width: "100vw",
-        height: "50vh",
-        bottom: "0", 
-        left: "0",
-        overflow: "hidden",
-        backgroundColor: "black"
-    }
-
-    const floatyStyles={
-        fontSize: "25px", 
-        fontFamily: "sans-serif", 
-        fontWeight: "bold", 
-        animation: "move 30s infinite alternate ease-in-out",
-        textShadow:` 
-        0 1px 0 #cccccc, 
-        0 2px 0 #cccccc, 
-        0 3px 0 #cccccc;`
-    }
-
-    const possibleColors = [
-        "#22c1c3", 
-        "#fdbb2d", 
-        "#c746ce",
-        "#48ce46",
-        // "#428bff"
-    ]
-
-    const goFloats = [null, null, null, null]
 
     return (
         <div className="landingPage">
@@ -47,39 +17,8 @@ const LandingPage = (props) => {
                 {...props}
             />
             <main className="main">
-                <div className="shapes-container">
-                    <div className="left-ring"></div>
-                    <div className="right-ring"></div>
-                    <div className="circle"></div>
-                    <div className="top-blob"></div> 
-                    <div className="giant-dollar-wrapper">
-                        <h1 className="giant-dollar">$</h1>
-                    </div>
-                    <div styles={floatyContainer}>
-                        {/* {goFloats.map((elem, index, arr) => {
-                            const color = possibleColors[Math.floor(Math.random() * 4)]
-                            const styles={
-                                ...floatyStyles,
-                                color: color,
-                            }
-                            return <p key={3000 + index} style={styles}>$</p>
-                        })} */}
-                    </div>
-                    
-                </div>
-                <div className="hero-wrapper">
-                    {/* <div className="row"> */}
-                        <div className="text-container">
-                            <h1 className="title">
-                                Money Buddy
-                                <span className="sub-title"> for personal finance </span>
-                            </h1>
-                            <p className="paragraph">
-                                A collection of powerful micro-tools that put you in command of your money.
-                            </p>
-                        </div>
-                    {/* </div> */}
-                </div>
+                <ShapesContainer />
+                <Hero />
                 <MobileNavBar 
                     {...props}
                 />
@@ -88,7 +27,20 @@ const LandingPage = (props) => {
     )
 }
 
-export default LandingPage;
+export default LandingPage
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
