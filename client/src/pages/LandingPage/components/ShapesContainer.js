56dd8f25601e6d8436ba9369aabb5c34f9f84f94
Collin Didier <collin.didier80@gmail.com>
2021-02-03 21:51:54 -0600

    create component for the shapes container on landing page

---
+++ client/src/pages/LandingPage/components/ShapesContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ShapesContainer = () => {
+    return (
+        <div className="shapes-container">
+            <div className="left-ring"></div>
+            <div className="right-ring"></div>
+            <div className="circle"></div>
+            <div className="top-blob"></div> 
+            <div className="giant-dollar-wrapper">
+                <h1 className="giant-dollar">$</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ShapesContainer
