b64c5fb58f43be67da788572fb61e4172fdd5394
Collin Didier <collin.didier80@gmail.com>
2021-02-04 21:16:00 -0600

    production prep:  del console logs and comments

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/modules/cellFunctions.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/modules/cellFunctions.js
@@ -1,6 +1,5 @@
 export const formatToCurrency = (number) => {
     try {
-        // console.log(number)
         let formattedCell = number.toLocaleString('en-US', { 
             style: 'currency', 
             currency: 'USD',
@@ -10,7 +9,6 @@ export const formatToCurrency = (number) => {
         return formattedCell
         
     } catch (error) {
-        // console.log(error)
         throw(error)
     }
 }
@@ -19,20 +17,15 @@ export const formatToCurrency = (number) => {
 export const filterNumbers = (e, updateRawNumber) => {
     const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     const { value } = e.target
-    // convert users blank entries to 0 values
     const userClearedCell = (value === "")
     if (userClearedCell) {
         updateRawNumber(0)
         return
     }
-
-    // do nothing if user enters a non-number character
     const invalidCharacterEntered = !(numbers.includes(value.charAt(value.length-1)))
     if (invalidCharacterEntered) {
         return
     }
-
-    // don't allow numbers to lead with 0s unless 0 is the value of the cell
     const invalidLeadingZero = (value.length > 1 && value.charAt(0) === "0")
     if (invalidLeadingZero) {
         updateRawNumber(value.charAt(1))
