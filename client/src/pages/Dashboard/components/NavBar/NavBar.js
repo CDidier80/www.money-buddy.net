764f25c3e07a4ef1232019059f9bbae87e295f03
Collin Didier <collin.didier80@gmail.com>
2021-02-05 02:58:06 -0600

    refactor jsx for nice contour and readability

--- client/src/pages/Dashboard/components/NavBar/NavBar.js
+++ client/src/pages/Dashboard/components/NavBar/NavBar.js
@@ -1,24 +1,17 @@
 import AccountPageLink from "./components/AccountPageLink"
 import LandingPageLink from "./components/LandingPageLink"
 import SignOutLink from "./components/SignOutLink"
-import React, {useState, useEffect} from 'react'
 import Burger from "./components/Burger"
+import React from 'react'
 
 const NavBar = (props) => {
 
     return (
         <nav className="dash-navbar">
             <LandingPageLink />
-            <Burger 
-                {...props}
-            />
-            {/* <h2 style={{color:"white", fontSize:"25px"}}>{size}</h2> */}
-            <SignOutLink 
-                {...props}
-            />
-            <AccountPageLink 
-                {...props}
-            />
+            <Burger {...props} />
+            <SignOutLink {...props} />
+            <AccountPageLink {...props} />
         </nav>
     )
 }
