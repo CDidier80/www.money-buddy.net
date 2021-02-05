bdb247bd632f94cb0f9ffa53a8c27d2ecb249889
Collin Didier <collin.didier80@gmail.com>
2021-02-04 21:13:52 -0600

    production prep:  del console logs and comments

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/modules/incomeSortFunctions.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/modules/incomeSortFunctions.js
@@ -1,15 +1,11 @@
 const divideDefaultsAndUserSources = (incomesArrayCopy) => {
     let userIncomes = []
     let sources = []
-    // console.log("incomesArrayCopy", incomesArrayCopy)
     for (let i = 0; i <= incomesArrayCopy.length -1; i++){
-        // divide the incomesArrayCopy into arrays of defaultIncome items and
-        // non default items 
         const { source } = incomesArrayCopy[i]
         const defaultOccurence = source.indexOf("income source #")
         if (defaultOccurence === 0){
             const lastCharNumber = parseInt(source.charAt(source.length - 1))
-            console.log("lastCharNumber:", lastCharNumber)
             sources.push(lastCharNumber)
         } else {
             userIncomes.push(incomesArrayCopy[i])
@@ -20,9 +16,6 @@ const divideDefaultsAndUserSources = (incomesArrayCopy) => {
 
 
 const insertionSort = (sources) => {
-    // console.log("userIncomes:", userIncomes)
-    // console.log("sources before sort: ", sources)
-    // insertion-sort the sources array
     for (let i = 1; i < sources.length; i++) {
         let currentIndex = i
         let valueToSort = sources[currentIndex]
@@ -38,16 +31,10 @@ const insertionSort = (sources) => {
 
 
 const findGapInNumbers = (sources) => {
-    // console.log("sources: ", sources)
-    // [1,2,3,4,7,9] ==>  looking for non-sequential numbers
-    // indices   [0,1,2,3,4,5]      if value at index is not +1 higher, there's a break
     const iterCount = sources.length-1
-    // console.log(iterCount, "itercount")
     for (let i = 0; i <= iterCount; i++) {
-        console.log("iteration count:", i)
         if (i+1 === sources[i]) {
             if(i === iterCount){
-                console.log("pushing to array end")
                 sources.push(i+2)
             } else {
                 continue
@@ -63,6 +50,6 @@ const findGapInNumbers = (sources) => {
 
 module.exports = {
     divideDefaultsAndUserSources, 
+    findGapInNumbers,
     insertionSort,
-    findGapInNumbers
 }
