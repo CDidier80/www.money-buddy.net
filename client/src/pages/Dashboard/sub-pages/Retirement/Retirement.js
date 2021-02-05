6caf4244f00696cc1344ccabb2fa55a507907bbb
Collin Didier <collin.didier80@gmail.com>
2021-02-04 21:19:01 -0600

    production prep: del commented out code, del unused import

--- client/src/pages/Dashboard/sub-pages/Retirement/Retirement.js
+++ client/src/pages/Dashboard/sub-pages/Retirement/Retirement.js
@@ -1,11 +1,7 @@
 import RetirementChart     from  "./components/RetirementChart/RetirementChart"
-import StateUpdateTest     from  "./components/ChartControls/StateUpdateTest"
 import ChartControls       from  "./components/ChartControls/ChartControls"
 import PageHeader          from  "./components/PageHeader/PageHeader"
-import React, { 
-    useState, 
-    useEffect 
-} from  "react"
+import React, { useState } from  "react"
 import "./styles/retirement.css"
 
 const Retirement = (props) => {
@@ -27,22 +23,6 @@ const Retirement = (props) => {
     const [salaryGrowthRate, setSalaryGrowthRate] = useState(2)
     const [retirementSpending, setRetirementSpending] = useState(50000)
 
-
-    /* ------------------------- Listen to Window Size ------------------------ */
-
-    // const [pageHeight, setPageHeight] = useState(window.innerHeight)
-    // const [pageWidth, setPageWidth] = useState(window.innerWidth)
-
-    // const setWindowSize = (e) => {
-    //     setPageHeight(window.innerHeight)
-    //     setPageWidth(window.innerWidth)
-    // }
-    
-    // useEffect(() => {
-    //     window.addEventListener("resize", setWindowSize)
-    //     return () => window.removeEventListener("resize", setWindowSize)
-    // }, [])
-    
 
     /* ---------------------- PROPS FOR CHILDREN --------------------- */
 
@@ -70,19 +50,12 @@ const Retirement = (props) => {
         setRetirementSpending,
     }
 
-    // const sizes = {
-    //     pageWidth,
-    //     pageHeight,
-    // }
-
-
     return (
         <div className="retirement">
             <PageHeader />
             <RetirementChart 
                 {...props}
                 fromRetirement={{...hookValues}}
-                // sizes={{...sizes}}
             />
             <ChartControls 
                 {...props}
