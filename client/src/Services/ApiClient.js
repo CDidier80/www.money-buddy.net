9a5e14e9d7402997f5026976fb569d32a8d06793
Collin Didier <collin.didier80@gmail.com>
2021-02-04 20:58:49 -0600

    production prep: delete interceptor notes

--- client/src/Services/ApiClient.js
+++ client/src/Services/ApiClient.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
-const url = process.env.NODE_ENV === 'production' ?  `${window.location.origin}/api` : 'http://localhost:3001/api'   // <-- for deployment ?  
-// console.log("API BASELINE URL: ", url)
+const url = process.env.NODE_ENV === 'production' ?  `${window.location.origin}/api` : 'http://localhost:3001/api'  
 
 const ApiClient = axios.create({ baseURL: url }) 
 
-// used only after user is logged in and authenticated. 
-// They maintain authenticated status by checking the validity of the current token
 ApiClient.interceptors.request.use(
     async (config) => {
     const token = localStorage.getItem('token')
@@ -14,13 +11,8 @@ ApiClient.interceptors.request.use(
     return config
     },
     (err) => {
-        // console.log("interceptors found auth failed")
         return Promise.reject(err)
 })
 
 export default ApiClient
 
-
-    // console.log("interceptors fired")
-    // console.log("token plucked from local storage: ", token)
-    // console.log("config being returned:", config)
