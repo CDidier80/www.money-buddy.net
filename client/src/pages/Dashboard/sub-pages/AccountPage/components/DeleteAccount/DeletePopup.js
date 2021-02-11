bed73fa98c3f0adb7cc5b3a5056b9a469c264e33
Collin Didier <collin.didier80@gmail.com>
2021-02-11 14:40:33 -0600

    fix auth/valid session props on delete account popup

--- client/src/pages/Dashboard/sub-pages/AccountPage/components/DeleteAccount/DeletePopup.js
+++ client/src/pages/Dashboard/sub-pages/AccountPage/components/DeleteAccount/DeletePopup.js
@@ -3,19 +3,30 @@ import React from 'react';
 
 const DeletePopup = (props) => {
 
-    const {userId, history, setDeleteTriggered} = props
+    const {
+        userId, 
+        history, 
+        setDeleteTriggered
+    } = props
+
+    const {
+        setAuth,
+        setSessionValid,
+    } = props.fromApp
 
     const deleteAccount = async (e) => {
         e.preventDefault()
-        console.log(userId)
         await DeleteUser({userId})
+        localStorage.clear()
+        setAuth(false)
+        setSessionValid(false)
         history.push("/")
     }
 
     return (
         <div 
             className="customBackdrop" 
-            onClick={(e) => setDeleteTriggered(false)} 
+            onClick={() => setDeleteTriggered(false)} 
         >
             <div className="popup">
                 <p className="warning">Warning: This action is irreversible.</p>
