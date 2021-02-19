a0a7167978c538f5f437011627d809fb516b5885
Collin Didier <collin.didier80@gmail.com>
2021-02-19 00:31:53 -0600

    use mui theme on sign in sign up page

--- client/src/pages/SignInSignUp/SignInSignUpPage.js
+++ client/src/pages/SignInSignUp/SignInSignUpPage.js
@@ -1,15 +1,22 @@
 
 import Form from "./components/Form/Form"
 import "./styles/signInSignUp.css"
+import makeLandingStyles from "./styles/styles"
+import { withTheme } from '@material-ui/core/'
 import React from 'react';
 
 const SignInSignUpPage = (props) => {
 
+
+    const useLandingStyles = makeLandingStyles(props.theme)
+    const classes = useLandingStyles()
+
+
     return (
-        <div className="sign-in-sign-up-page">
+        <div className={classes.signInSignUpPage}>
             <Form {...props} />
         </div>
     )
 }
 
-export default SignInSignUpPage
+export default withTheme(SignInSignUpPage)
