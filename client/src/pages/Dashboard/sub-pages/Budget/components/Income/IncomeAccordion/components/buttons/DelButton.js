a597d1125a92ff7675923b917fb820230113a2b1
Collin Didier <collin.didier80@gmail.com>
2021-02-21 20:30:44 -0600

    comment out button temporarily

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeAccordion/components/buttons/DelButton.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeAccordion/components/buttons/DelButton.js
@@ -1,43 +1,18 @@
-import { Button, makeStyles } from '@material-ui/core/'
-import React from 'react'
+// import { Button } from '@material-ui/core/'
+// import React from 'react'
 
-const DelButton = (props) => {
-
-    const {
-        smallerButtons,
-        showIncomeDeleteIcons,
-        toggleIncomeDeleteIcons,
-    } = props.fromIncomeAccordion
-    
-
-    const handleDeleteIncomeIcons = (e) => {
-        e.preventDefault()
-        toggleIncomeDeleteIcons(!showIncomeDeleteIcons)
-    }
+// const DelButton = (props) => {
 
 
-    const useStyles = makeStyles({
-        deleteButton: {
-            fontWeight: "700",
-            padding: "0 5px 0 5px",
-            fontFamily: "Lato, sans-serif",
-            fontSize: smallerButtons ? "8px" : "9px",
-            color: showIncomeDeleteIcons ? "#e6a824" : "#2c7b71",
-        }
-    })
 
-    
-    const classes = useStyles()
+//     return (
+//         <Button 
+//             className={classes.deleteButton}
+//             onClick={(e) => handleDeleteIncomeIcons(e)}
+//         >
+//             {showIncomeDeleteIcons ? "Cancel Delete" : "Delete Income"}
+//         </Button>
+//     )
+// }
 
-
-    return (
-        <Button 
-            className={classes.deleteButton}
-            onClick={(e) => handleDeleteIncomeIcons(e)}
-        >
-            {showIncomeDeleteIcons ? "Cancel Delete" : "Delete Income"}
-        </Button>
-    )
-}
-
-export default DelButton
+// export default DelButton
