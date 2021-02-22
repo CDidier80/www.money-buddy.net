1ac06752426fc05281beea281961af7f62ba5a9d
Collin Didier <collin.didier80@gmail.com>
2021-02-21 20:43:34 -0600

    move content to separate componenets

--- client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/components/SummaryTable/SummaryTable.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/components/SummaryTable/SummaryTable.js
@@ -1,45 +1,14 @@
-import { 
-    Paper, 
-    Table, 
-    TableBody, 
-    makeStyles,
-    TableContainer, 
-} from '@material-ui/core'
-import React from 'react'
 import SummaryRows from "../SummaryRows/SummaryRows"
+import TableWrapper from './TableWrapper'
+import React from 'react'
 
 
 const SummaryTable = (props) => {
 
-
-
-    const useStyles = makeStyles({
-        tableContainer: {
-            flexGrow: "2",
-            overflow: "hidden"
-        },
-        table: {
-            flexGrow: "1",
-            minHeight: "100%"
-        }
-    })
-
-    const classes = useStyles()
-
     return (
-        <TableContainer 
-            className={classes.tableContainer}
-            component={Paper}
-        >
-            <Table 
-                aria-label="a dense table"
-                className={classes.table}
-            >
-                <TableBody>
-                    <SummaryRows {...props} />
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <TableWrapper {...props} >
+            <SummaryRows {...props} />
+        </TableWrapper>
     )
 }
 
