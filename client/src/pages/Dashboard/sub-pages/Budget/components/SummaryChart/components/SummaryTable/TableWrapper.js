7a1f5492f9e0ebcb13a16b56752e7c79ca79ad50
Collin Didier <collin.didier80@gmail.com>
2021-02-21 20:44:13 -0600

    add tablewrapper component

---
+++ client/src/pages/Dashboard/sub-pages/Budget/components/SummaryChart/components/SummaryTable/TableWrapper.js
@@ -0,0 +1,42 @@
+import { 
+    Paper, 
+    Table, 
+    TableBody, 
+    makeStyles,
+    TableContainer, 
+} from '@material-ui/core'
+import React from 'react'
+
+export const TableWrapper = (props) => {
+
+    const useStyles = makeStyles({
+        tableContainer: {
+            overflow: "hidden",
+            flexGrow: "2",
+        },
+        table: {
+            minHeight: "100%",
+            flexGrow: "1",
+        }
+    })
+
+    const classes = useStyles()
+
+    return (
+        <TableContainer 
+            className={classes.tableContainer}
+            component={Paper}
+        >
+            <Table 
+                aria-label="a dense table"
+                className={classes.table}
+            >
+                <TableBody>
+                    {props.children}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default TableWrapper
