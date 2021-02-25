b4c62964d2bc7a20a6008d99bfacb0dced9320c5
Collin Didier <collin.didier80@gmail.com>
2021-02-25 01:33:54 -0600

    refactor with theme based styles

--- client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/configData/data.js
+++ client/src/pages/Dashboard/sub-pages/Retirement/components/RetirementChart/configData/data.js
@@ -1,29 +1,46 @@
+import { moneyBuddyTheme } from "../../../../../../../modules/themeAndStyles"
+const { primary, secondaryDark, secondary } = moneyBuddyTheme.palette
+
+const chartPoints = {
+    colors: {
+        pointBackgroundColor: primary.main,
+        pointBorderColor: secondaryDark.main,
+        pointHoverBorderColor: secondary.main,
+        pointHoverBackgroundColor: primary.main,
+    },
+    sizes: {
+        pointHoverBorderWidth: 2,
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHitRadius: 10,
+        pointRadius: 2,
+    }
+}
+
+const chartLine = {
+    other: {
+        borderDash: [],
+        lineTension: 0.2,
+        borderDashOffset: 0.0,
+        borderCapStyle: 'butt',
+        borderJoinStyle: 'miter',
+    },
+    color: { borderColor: secondary.main },
+}
+
 export const universalProperties = {
-    pointRadius: 2,
-    borderDash: [],
-    lineTension: 0.2,
-    pointHitRadius: 10,
-    pointBorderWidth: 1,
-    pointHoverRadius: 5,
-    borderDashOffset: 0.0,
-    borderCapStyle: 'butt',
-    borderJoinStyle: 'miter',
-    pointHoverBorderWidth: 2,
-    pointBorderColor: 'black',
-    pointBackgroundColor: 'white',
-    pointHoverBorderColor: '#0a6666',
-    pointHoverBackgroundColor: '#fdbb2d',
-    backgroundColor: "rgba(232, 184, 30, .35)",
+    backgroundColor: primary.transparent["4"],
+    ...chartPoints.colors,
+    ...chartPoints.sizes,
+    ...chartLine.color,
+    ...chartLine.other,
 }
 
 export const generateDataSets = (savingsForecast) => (
-    [
-        {
+    [{
             label: "", 
             fill: true,
             data: savingsForecast,
             ...universalProperties,
-            borderColor: 'rgba(10,102,102,.7)',
-        }
-    ]
+        }]
 )
