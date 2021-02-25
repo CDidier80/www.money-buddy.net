d0f3802759648405dea57e396e0361093d00e676
Collin Didier <collin.didier80@gmail.com>
2021-02-25 01:28:16 -0600

    reformat and beautify

--- models/outflows.js
+++ models/outflows.js
@@ -1,42 +1,48 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Outflow extends Model {
-    static associate(models) {
-      Outflow.belongsTo(models.Flowcategory, {
-        foreignKey: 'flowcategory_id',
-        as: 'outflows',
-        constraints: true,
-      })
+
+    class Outflow extends Model {
+
+        static associate(models) {
+            Outflow.belongsTo(models.Flowcategory, {
+                foreignKey: 'flowcategory_id',
+                as: 'outflows',
+                constraints: true,
+            })
+        }
     }
-  };
-  Outflow.init({
-    flowcategoryId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'flowcategory_id',
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
-      references: {
-        model: 'flowcategories',
-        key: 'id',
-      }
-    },
-    outflow: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-  }, 
-  {
-    sequelize,
-    modelName: 'Outflow',
-    tableName: 'outflows'
-  });
-  return Outflow;
-};
+
+    Outflow.init({
+        flowcategoryId: {
+            type: DataTypes.INTEGER,
+            field: 'flowcategory_id',
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+            allowNull: false,
+            references: {
+                model: 'flowcategories',
+                key: 'id',
+            }
+        },
+
+        outflow: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    }, 
+    {
+        sequelize,
+        modelName: 'Outflow',
+        tableName: 'outflows'
+    })
+
+    return Outflow
+}
