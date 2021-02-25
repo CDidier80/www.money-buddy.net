264e32e35b819045a8440ab40ab183318bb2a58a
Collin Didier <collin.didier80@gmail.com>
2021-02-25 01:28:55 -0600

    reformat and beautify

--- models/flowcategory.js
+++ models/flowcategory.js
@@ -1,41 +1,46 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  class Flowcategory extends Model {
-    static associate(models) {
-      Flowcategory.belongsTo(models.Month, {
-        foreignKey: 'month_id',
-        as: 'flowcategories',
-        constraints: true,
-      }),  
-      Flowcategory.hasMany(models.Outflow, {
-        foreignKey: 'flowcategory_id',
-        as: 'outflows',
-      })
-    }
-  };
-  Flowcategory.init({
-    monthId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'month_id',
-      onDelete: 'cascade',
-      references: {
-        model: 'months',
-        key: 'id'
+
+    class Flowcategory extends Model {
+
+      static associate(models) {
+
+          Flowcategory.belongsTo(models.Month, {
+            foreignKey: 'month_id',
+            as: 'flowcategories',
+            constraints: true,
+          }),  
+
+          Flowcategory.hasMany(models.Outflow, {
+            foreignKey: 'flowcategory_id',
+            as: 'outflows',
+          })
+        }
       }
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-      }
-  }, 
-  {
-    sequelize,
-    modelName: 'Flowcategory',
-    tableName: 'flowcategories'
-  });
-  return Flowcategory;
-};
+      Flowcategory.init({
+        monthId: {
+          type: DataTypes.INTEGER,
+          onDelete: 'cascade',
+          field: 'month_id',
+          allowNull: false,
+          references: {
+              model: 'months',
+              key: 'id'
+          }
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, 
+    {
+        sequelize,
+        modelName: 'Flowcategory',
+        tableName: 'flowcategories'
+    })
+
+    return Flowcategory
+}
