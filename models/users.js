332952741311ae91c9a151aa705cb2d372ac2008
Collin Didier <collin.didier80@gmail.com>
2021-02-25 01:26:39 -0600

    reformat and beautify

--- models/users.js
+++ models/users.js
@@ -1,28 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-
-    static associate(models) {
-      User.hasOne(models.Budget, {
-        foreignKey: 'user_id',
-        as: 'user_budget',
-      }),
-      User.hasOne(models.Cashflow, {
-        foreignKey: 'user_id',
-        as: 'user_cashflow',
-      })    
+    
+    class User extends Model {
+        static associate(models) {
+            User.hasOne(models.Budget, {
+                foreignKey: 'user_id',
+                as: 'user_budget',
+            }),
+            User.hasOne(models.Cashflow, {
+                foreignKey: 'user_id',
+                as: 'user_cashflow',
+            })    
+        }
     }
-  };
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users'
-  });
-  return User;  
+
+    User.init({
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, 
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users'
+    })
+
+  return User
 }
