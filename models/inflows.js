3b02068ef58813b2dc5c654a157a33d5986e6602
Collin Didier <collin.didier80@gmail.com>
2021-02-25 01:28:38 -0600

    reformat and beautify

--- models/inflows.js
+++ models/inflows.js
@@ -1,41 +1,47 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
+
   class Inflow extends Model {
+
     static associate(models) {
-      Inflow.belongsTo(models.Month, {
-        foreignKey: 'month_id',
-        as: 'inflows',
-        constraints: true,
-      })
+        Inflow.belongsTo(models.Month, {
+            foreignKey: 'month_id',
+            constraints: true,
+            as: 'inflows',
+        })
     }
-  };
+  }
+
   Inflow.init({
-    monthId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,  
-      field: 'month_id',
-      onDelete: 'cascade',
-      references: {
-        model: 'months',
-        key: 'id'
-      }
-    },
-    source: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
+      monthId: {
+          type: DataTypes.INTEGER,
+          onDelete: 'cascade',
+          field: 'month_id',
+          allowNull: true,  
+          references: {
+              model: 'months',
+              key: 'id'
+          }
+      },
+
+      source: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
+
+      amount: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+      },
   }, 
+
   {
     sequelize,
     modelName: 'Inflow',
     tableName: 'inflows'
-  });
-  return Inflow;
-};
+  })
+
+  return Inflow
+}
