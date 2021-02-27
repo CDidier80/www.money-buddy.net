0604cfe80adaa187617809c8bae684130a9d5629
Collin Didier <collin.didier80@gmail.com>
2021-02-27 15:01:03 -0600

    replace styles with imported theme-based styles

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Inflows/InflowAccordion/components/AccordionDropdownTab.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Inflows/InflowAccordion/components/AccordionDropdownTab.js
@@ -1,47 +1,27 @@
 import { 
-    makeStyles,
     Typography,
     AccordionSummary,
 } from '@material-ui/core'
 import React from 'react'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-
+import { useInflowsDropdownStyles } from "../../../styles/styles"
 
 const AccordionDropdownTab = (props) => {
 
-    const useStyles = makeStyles({
-        heading: {
-            fontFamily: "Lato, sans-serif",
-            paddingLeft: "27px",
-            fontWeight: "700",
-            margin: "0 auto",
-            fontSize: "18px",
-            color: "#22c1c3",
-            textShadow: `
-                0 0 1px lightgray;
-            `
-        },
-        expandMoreIcon : {
-            fontSize: "18px",
-            color:"#22c1c3",
-            paddingLeft: "0"
-        }
-    })
-
-    const classes = useStyles()
+    const {heading, expandMoreIcon} = useInflowsDropdownStyles(props.theme)
     
     return (
         <AccordionSummary
             expandIcon={
                 <ExpandMoreIcon 
-                    className={classes.expandMoreIcon}
+                    className={expandMoreIcon}
                 />
             }
             aria-controls="panel1a-content"
             id="panel1a-header"
         >
             <Typography 
-                className={classes.heading}
+                className={heading}
             >
                 Inflows
             </Typography>
