630d96d525dd40aa3fd8754e75b79bd81db01cb9
Collin Didier <collin.didier80@gmail.com>
2021-02-27 15:00:15 -0600

    add theme based styles into their own module for import

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/InDevelopmentPopup/PopupHeader.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/InDevelopmentPopup/PopupHeader.js
@@ -1,27 +1,16 @@
 import { DialogTitle, makeStyles } from '@material-ui/core'
+import { usePopupHeaderStyles } from "./modalStyles"
 import React from 'react'
 
 
-const PopupHeader = () => {
-
-    const useStyles = makeStyles({
-
-        header: {
-            fontFamily: "Lato, sans-serif",
-            padding: "20 20 0 20px",
-            textAlign: "center",
-            fontWeight: "700",
-            fontSize: "20px",
-            color: "#22c1c3",
-        }
-    })
+const PopupHeader = ({theme}) => {
     
-    const classes = useStyles()
+    const { header } = usePopupHeaderStyles(theme)
 
     return (
         <DialogTitle 
             id="alert-dialog-slide-title"
-            className={classes.header}
+            className={header}
             disableTypography={true}
         > 
             A quick note...
