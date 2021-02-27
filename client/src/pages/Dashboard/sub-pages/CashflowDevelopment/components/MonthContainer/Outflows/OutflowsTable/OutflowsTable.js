7222962541f0f51f050e30cfbd70c9650705f832
Collin Didier <collin.didier80@gmail.com>
2021-02-27 15:10:46 -0600

    reformat imports move components into modular subcomponents for import

--- client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Outflows/OutflowsTable/OutflowsTable.js
+++ client/src/pages/Dashboard/sub-pages/CashflowDevelopment/components/MonthContainer/Outflows/OutflowsTable/OutflowsTable.js
@@ -1,79 +1,19 @@
-import { 
-    Paper, 
-    Table, 
-    TableBody, 
-    makeStyles,
-    TableContainer, 
-    AccordionDetails,
-} from '@material-ui/core'
 import React, { useState } from 'react'
-import OutflowRow from "../OutflowRow/OutflowRow"
+import TableWrapper from "./TableWrapper"
+import { TableBody } from '@material-ui/core'
+import OutflowRows from "../OutflowRow/OutflowRows"
 import OutflowHeaders from "./components/OutflowHeaders"
-import { offRowColor } from "../../../../../universal-functions/styleFunctions"
 
 
 const OutflowTable = (props) => {
 
-    /*  PROPS */
-
-    const { flowcategory } = props.fromOutflowsAccordion
-    const { outflows } = flowcategory
-
-     /*  STATE */
-
-    const [incomingDeletion, setIncomingDeletion] = useState(false)
-
-
-    /*  FUNCTIONS */
-
-    const useStyles = makeStyles({
-        tableBody: { overflow: "hidden" },
-        tableContainer: {
-            boxShadow: "1px -1px 1px rgb(0, 0, 0, 0.1)",
-            padding: '4px',
-            margin: "auto",
-        },
-    })
-
-    const classes = useStyles()
-
     return (
-        <AccordionDetails
-            className={classes.tableContainer}
-        >
-            <TableContainer component={Paper}>
-                <Table 
-                    className={classes.table} 
-                    aria-label="a dense table"
-                    size="small" 
-                >
-                    <TableBody >
-                        <OutflowHeaders 
-                            {...props}
-                        />
-                        {outflows.map((outflowObj, index) => {
-                            const { outflow, amount } = outflowObj
-                            const rowColor = offRowColor(index)                            
-                            const propsForRows = {
-                                setIncomingDeletion,
-                                outflowIndex: index,
-                                incomingDeletion, 
-                                outflow,
-                                amount, 
-                                rowColor
-                            }
-                            return (
-                                <OutflowRow 
-                                    {...props}
-                                    key={`${index * -1}`} 
-                                    fromOutflowsTable={{...propsForRows}}
-                                /> 
-                            )
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </AccordionDetails>
+        <TableWrapper>
+            <TableBody>
+                <OutflowHeaders {...props} />
+                <OutflowRows    {...props} />
+            </TableBody>
+        </TableWrapper>
     )
 }
 
