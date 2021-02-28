556fb08cdfeac06c5a21354c7e0f7e46ca89dbf4
Collin Didier <collin.didier80@gmail.com>
2021-02-28 15:29:16 -0600

    replace mui base table cells with custom cell components move style logic into new custom components

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/IncomeHeaders/IncomeHeaders.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/IncomeHeaders/IncomeHeaders.js
@@ -1,56 +1,30 @@
-import { TableCell, TableRow  } from '@material-ui/core'
+import { TableRow  } from '@material-ui/core'
 import CellButtonGroup from "./buttons/CellButtonGroup"
-import { useIncomeHeaderStyles } from "../styles"
+import HeaderCell from "./cells/HeaderCell"
+import EmptyCell from "./cells/EmptyCell"
 import React from 'react';
 
 
 
 const IncomeHeaders = (props) => {
 
-    const { onlyTwoCells, textSize: ts } = props
 
-    const smallerWidth = props.onlyTwoCells ? {width: "70x"} : {}
-    const textSize = ts ? ts : {}
-
-    const inlineHeaderStyles = { ...smallerWidth, ...textSize }
-
-    const classes = useIncomeHeaderStyles(props.theme)
-    
     return (
-        // <TableHead>
             <TableRow>
-                <TableCell 
-                    className={classes.emptyCell}>
-                </TableCell>
-                <TableCell 
-                    className={classes.columnHeader}
-                    style={inlineHeaderStyles}
-                >
-                    Income Source
-                </TableCell>
+                <EmptyCell />
+                
+                <HeaderCell {...props} label="Income Source" />
+                
                 {props.onlyTwoCells ? 
                     <CellButtonGroup {...props} />
                 :
                     <>
-                        <TableCell 
-                            className={classes.columnHeader} 
-                            style={inlineHeaderStyles}
-                            align="right"
-                        >
-                            Monthly Average
-                        </TableCell>
-                        <TableCell 
-                            className={classes.columnHeader} 
-                            style={inlineHeaderStyles}
-                            align="right"
-                        >
-                                Annual
-                        </TableCell>
+                        <HeaderCell {...props} label="Monthly Average" />    
+                        <HeaderCell {...props} label="Annual" />  
                     </>
                 }
             </TableRow>
     )
 }
 
-{/* </TableHead> */}
 export default IncomeHeaders
