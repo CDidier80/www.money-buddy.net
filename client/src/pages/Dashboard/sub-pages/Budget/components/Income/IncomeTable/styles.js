06de9ee51b6893fe1cc8af9e76288ac80886f4fd
Collin Didier <collin.didier80@gmail.com>
2021-02-28 15:33:14 -0600

    add responsive styles to style exports

--- client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/styles.js
+++ client/src/pages/Dashboard/sub-pages/Budget/components/Income/IncomeTable/styles.js
@@ -1,18 +1,51 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useMediaQuery } from '@material-ui/core'
+const nssr = {noSsr: true}
 
-export const useIncomeHeaderStyles = makeStyles(theme => {
 
-    const { primaryDark: pd } = theme.palette
+export const useIncomeHeaderStyles = (props) => {
 
-    return ({
-        columnHeader: {
-            color: pd.main,
-            fontWeight: "bold",
-        },
-        emptyCell: {
-            maxWidth: "36px",
-            minWidth: "36px", 
-            padding: "0px"
-        }
+    const { theme, textSize } = props
+    
+    const onlyTwoCells = useMediaQuery('(max-width: 390px)', nssr)
+
+
+
+    const responsiveStyles = {
+        ...(onlyTwoCells ? { width: "70px"} : {}),
+        ...(textSize ? textSize : {})
+
+    }
+
+    const useStyles = makeStyles(({palette: p}) => {
+
+        return ({
+            headerCell: {
+                color: p.primaryDark.main,
+                ...responsiveStyles,
+                fontWeight: "bold",
+            },
+
+        })
     })
-})
+
+    return useStyles(theme)
+}
+
+
+
+export const useEmptyCellStyles = (theme) => {
+
+
+    const useStyles = makeStyles(theme => {
+
+        return({
+            emptyCell: {
+                maxWidth: "36px",
+                minWidth: "36px", 
+                padding: "0px"
+            }
+        })
+    })
+
+    return useStyles(theme)
+}
