299debf198b74c06e1e3085903bd5cffbb52e500
Collin Didier <collin.didier80@gmail.com>
2021-03-11 03:41:12 -0600

    add filterNumbers function for client-wide export

--- client/src/modules/clientFunctions.js
+++ client/src/modules/clientFunctions.js
@@ -61,3 +61,29 @@ export class Helpers {
         console.log(`${name}: `, varObj[name])
     }
 }
+
+
+export const filterNumbers = (e, updateRawNumber) => {
+    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const { value } = e.target
+    // convert users blank entries to 0 values
+    const userClearedCell = (value === "")
+    if (userClearedCell) {
+        updateRawNumber(0)
+        return
+    }
+
+    // do nothing if user enters a non-number character
+    const invalidCharacterEntered = !(numbers.includes(value.charAt(value.length-1)))
+    if (invalidCharacterEntered) {
+        return
+    }
+
+    // don't allow numbers to lead with 0s unless 0 is the value of the cell
+    const invalidLeadingZero = (value.length > 1 && value.charAt(0) === "0")
+    if (invalidLeadingZero) {
+        updateRawNumber(value.charAt(1))
+        return
+    }
+    updateRawNumber(value)
+}
