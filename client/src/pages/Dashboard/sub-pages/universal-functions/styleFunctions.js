7e7867990dcd37cb7b07baccd1dd4b144617f9ea
Collin Didier <collin.didier80@gmail.com>
2021-03-17 21:38:24 -0500

    del misplaced file

--- client/src/pages/Dashboard/sub-pages/universal-functions/styleFunctions.js
+++
@@ -1,4 +0,0 @@
-import { moneyBuddyTheme } from "../../../../modules/themeAndStyles"
-const { rowColor, offRowColor} = moneyBuddyTheme
-
-export const pickColor = (index) => (index % 2 === 0) ? rowColor : offRowColor
