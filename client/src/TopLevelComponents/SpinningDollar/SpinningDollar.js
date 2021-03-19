faef4ab00e383255ef5a51a2c1d2632178d1f40a
Collin Didier <collin.didier80@gmail.com>
2021-03-19 16:11:06 -0500

    add loading spinner for app-wide use

---
+++ client/src/TopLevelComponents/SpinningDollar/SpinningDollar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./spinningDollar.css"
+
+const SpinningDollar = () => {
+    return (
+        <div className="spinner">
+            <div className="rim"></div>
+            <div className="rim"></div>
+            <div className="dollar">$</div>
+        </div>
+    )
+}
+
+export default SpinningDollar
