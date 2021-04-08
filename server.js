c2f548ab853a88ae4b94c90bf74b70dba49ccf84
Collin Didier <collin.didier80@gmail.com>
2021-04-08 17:17:52 -0500

    del duplicate use of cors

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
