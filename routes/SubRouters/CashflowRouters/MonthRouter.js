b2911bbdc42d8a2e5c3ca401ce83e8d43ef31f60
Collin Didier <collin.didier80@gmail.com>
2021-06-05 19:33:37 -0500

    Convert routers to typescript

--- routes/SubRouters/CashflowRouters/MonthRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router()
-const controller = require('../../../controllers/CashflowControllers/MonthController')
-const { GetMonth, GetMonths, CreateMonth } = controller
-
-Router.post('/create', CreateMonth)
-Router.get('/getmany', GetMonths)
-Router.get('/getone',  GetMonth)
-
-module.exports = Router
